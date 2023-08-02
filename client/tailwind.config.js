350dca05d06795d661103f7757eaa8e0e27cf733
bae-sh <hyeon1293@gmail.com>
2023-08-02 21:07:13 +0900

    chore: 공통 색상 tailwind 추가

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main-hyundai-blue': ' #0E2B5C',
+        'sub-active-blue': '#4CA7CE',
+        'icon-yellow': '#FFA724',
+        'tag-sky-blue': '#DEEBFF',
+        'cool-grey-001': '#F2F4F7',
+        'cool-grey-002': '#DBDEE3',
+        'cool-grey-003': '#AEB1B7',
+        'cool-grey-004': '#595C61',
+        'cool-grey-black': '#202732',
+      },
+    },
   },
   plugins: [],
 };
