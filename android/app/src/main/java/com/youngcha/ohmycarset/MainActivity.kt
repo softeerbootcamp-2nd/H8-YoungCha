8567de238429ac8fdf15fc05deed095d2de49a76
HyeonSeongKang <gt9697@naver.com>
2023-08-02 15:01:10 +0900

    chore: 프로젝트 세팅

---
+++ android/app/src/main/java/com/youngcha/ohmycarset/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.youngcha.ohmycarset
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
