17417170a78e5cf98a634647d0b435756bdaf929
1lsang <ilsangpark.98@gmail.com>
2023-08-02 15:06:33 +0900

    chore: Storybook 설치

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,32 +1,25 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:jsx-a11y/recommended',
-  ],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: ['.eslintrc.{js,cjs}'],
-      parserOptions: {
-        sourceType: 'script',
-      },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended', 'plugin:jsx-a11y/recommended', 'plugin:storybook/recommended'],
+  overrides: [{
+    env: {
+      node: true
     },
-  ],
+    files: ['.eslintrc.{js,cjs}'],
+    parserOptions: {
+      sourceType: 'script'
+    }
+  }],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['@typescript-eslint', 'react'],
   rules: {
-    'react/react-in-jsx-scope': 'off',
-  },
+    'react/react-in-jsx-scope': 'off'
+  }
 };
