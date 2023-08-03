298a5f10555c190928aa3c1b770f83629d56e074
bae-sh <b5460881@naver.com>
2023-08-03 14:08:16 +0900

    chore: typography 설정

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,20 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+import typography from './plugins/typography';
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        'main-hyundai-blue': ' #0E2B5C',
-        'sub-active-blue': '#4CA7CE',
+        'main-blue': ' #0E2B5C',
+        'sub-blue': '#4CA7CE',
         'icon-yellow': '#FFA724',
-        'tag-sky-blue': '#DEEBFF',
-        'cool-grey-001': '#F2F4F7',
-        'cool-grey-002': '#DBDEE3',
-        'cool-grey-003': '#AEB1B7',
-        'cool-grey-004': '#595C61',
-        'cool-grey-black': '#202732',
+        'tag-skyblue': '#DEEBFF',
+        'grey-001': '#F2F4F7',
+        'grey-002': '#DBDEE3',
+        'grey-003': '#AEB1B7',
+        'grey-004': '#595C61',
+        'grey-black': '#202732',
       },
     },
+    fontFamily: {
+      'hsans-head': ['Hyundai Sans Head KR', 'sans-serif'],
+      'hsans-text': ['HyundaiSansText', 'sans-serif'],
+    },
   },
-  plugins: [],
+  plugins: [typography],
 };
