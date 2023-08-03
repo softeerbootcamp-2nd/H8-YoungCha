dbf85e0482c512a78ed118bd5d8ae02bdceb8a37
dohyeon-han <limited.hyeon@gmail.com>
2023-08-03 18:03:52 +0900

    chore: 테스트 용 메소드 구현

---
+++ server/src/test/java/com/youngcha/server/car/CarTest.java
@@ -0,0 +1,14 @@
+package com.youngcha.server.car;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+
+    @Test
+    void test() {
+        Car car = new Car();
+        car.test();
+    }
+}
