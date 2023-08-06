d17f54236fd3f0290c97d6e6e28280e28b48f865
dohyeon-han <limited.hyeon@gmail.com>
2023-08-06 17:29:49 +0900

    feat: controller 및 dto에 swagger 관련 설정

--- server/src/main/java/com/youngcha/server/test/TestController.java
+++ server/src/main/java/com/youngcha/server/test/TestController.java
@@ -1,25 +1,44 @@
 package com.youngcha.server.test;
 
+import com.youngcha.server.common.dto.ErrorResponse;
 import com.youngcha.server.common.dto.SuccessResponse;
 import com.youngcha.server.common.exception.CustomException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Test API", description = "테스트 API")
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
+    @Operation(summary = "성공 테스트 메소드", description = "임시 성공 메소드입니다.")
     @GetMapping("/ok")
-    public ResponseEntity<SuccessResponse> test() {
+    public ResponseEntity<SuccessResponse<SuccessResponse<String>>> test(
+            @Parameter(description = "파라미터")
+            @RequestParam(required = false) String param) {
         SuccessResponse<String> response = new SuccessResponse<>("ok");
-        return ResponseEntity.ok(response);
+        SuccessResponse<SuccessResponse<String>> r = new SuccessResponse<>(response);
+        return ResponseEntity.ok(r);
     }
 
+    @Operation(summary = "실패 테스트 메소드", description = "임시 실패 메소드입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "400", description = "실패",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+    })
     @GetMapping("/error")
-    public ResponseEntity<SuccessResponse> error() {
+    public ResponseEntity<SuccessResponse<String>> error() {
         throw new CustomException(HttpStatus.BAD_REQUEST, "error");
     }
 }
