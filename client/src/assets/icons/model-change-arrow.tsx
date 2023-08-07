94b568229441df1c35f5a43efa35c37353cd33ee
jhyep <pjh1115_@naver.com>
2023-08-07 15:19:07 +0900

    chore: header에 사용되는 svg 컴포넌트화

---
+++ client/src/assets/icons/model-change-arrow.tsx
@@ -0,0 +1,21 @@
+function ModelChangeArrow() {
+  return (
+    <svg
+      width="18"
+      height="18"
+      viewBox="0 0 18 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="Group">
+        <path
+          id="Vector"
+          d="M9 11.998L4.5 7.49805L13.5 7.49805L9 11.998Z"
+          fill="#202732"
+        />
+      </g>
+    </svg>
+  );
+}
+
+export default ModelChangeArrow;
