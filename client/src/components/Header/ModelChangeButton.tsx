7949db50a168fe78d15c7e54f5aae75908bae05a
jhyep <pjh1115_@naver.com>
2023-08-07 15:23:13 +0900

    feat: 모델 변경 버튼 구현

---
+++ client/src/components/Header/ModelChangeButton.tsx
@@ -0,0 +1,13 @@
+import * as Icon from '../../assets/icons';
+
+function ModelChangeButton() {
+  return (
+    <button className="flex text-center title5">
+      <Icon.CarIcon />
+      <span className="w-[70px]">팰리세이드</span>
+      <Icon.ModelChangeArrow />
+    </button>
+  );
+}
+
+export default ModelChangeButton;
