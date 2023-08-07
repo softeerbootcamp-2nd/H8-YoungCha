635c9f5a83f0f61c5b53879c0e8e2d833d55cb12
bae-sh <b5460881@naver.com>
2023-08-07 20:15:06 +0900

    feat: OptionLayout 컴포넌트 구현

---
+++ client/src/pages/home/OptionLayout.tsx
@@ -0,0 +1,21 @@
+interface OptionLayoutProps {
+  title?: string;
+  children?: React.ReactNode;
+}
+
+function OptionLayout({ title, children }: OptionLayoutProps) {
+  return (
+    <div className="flex flex-col gap-[16px] max-w-7xl">
+      {title && (
+        <h3 className="font-medium text-center text-grey-black py-[8px]">
+          {title}
+        </h3>
+      )}
+      <ul className="flex justify-around gap-[48px] w-[1024px] px-[24px]">
+        {children}
+      </ul>
+    </div>
+  );
+}
+
+export default OptionLayout;
