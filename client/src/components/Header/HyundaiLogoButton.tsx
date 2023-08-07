560dab2094527bcdcff79153ca63a874d87b2436
jhyep <pjh1115_@naver.com>
2023-08-07 15:22:06 +0900

    feat: 헤더 로고 버튼 구현

---
+++ client/src/components/Header/HyundaiLogoButton.tsx
@@ -0,0 +1,11 @@
+import * as Icon from '../../assets/icons';
+
+function HyundaiLogoButton() {
+  return (
+    <button className="w-[149px] h-[21px] pr-[20px]">
+      <Icon.HyundaiLogo />
+    </button>
+  );
+}
+
+export default HyundaiLogoButton;
