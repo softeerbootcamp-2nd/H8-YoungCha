23ac175689bd7f211afcaeec10207c2396635411
jhyep <pjh1115_@naver.com>
2023-08-07 15:20:16 +0900

    feat: 백카사전 on/off 토글 버튼 구현

---
+++ client/src/components/Header/DictionaryToggleButton.tsx
@@ -0,0 +1,24 @@
+import * as Icon from '../../assets/icons';
+import { useState } from 'react';
+
+function DictionaryToggleButton() {
+  const [dictionaryOn, setDictionary] = useState<boolean>(false);
+
+  function toggleDictionary() {
+    setDictionary((prevState) => !prevState);
+  }
+  return (
+    <button className="flex text-center title5" onClick={toggleDictionary}>
+      {dictionaryOn ? <Icon.DictionaryOnIcon /> : <Icon.DictionaryOffIcon />}
+      <span
+        className={`pl-[4px] ${
+          dictionaryOn ? 'text-icon-yellow' : 'text-grey-black'
+        }`}
+      >
+        {dictionaryOn ? '백카사전 on' : '백카사전 off'}
+      </span>
+    </button>
+  );
+}
+
+export default DictionaryToggleButton;
