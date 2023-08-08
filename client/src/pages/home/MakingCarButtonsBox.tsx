2219eff623bed0d2a54a92b5c29b1a15689fa483
bae-sh <b5460881@naver.com>
2023-08-08 18:42:07 +0900

    chore: 공통 이름 상수로 변경

--- client/src/pages/home/MakingCarButtonsBox.tsx
+++ client/src/pages/home/MakingCarButtonsBox.tsx
@@ -1,19 +1,21 @@
 import OptionLayout from './OptionLayout';
 
+const MAKING_MY_CAR_TITLE = '내 차 만들기';
+
 function MakingCarButtonsBox() {
   return (
     <OptionLayout>
       <li className="flex items-center justify-center text-white w-140px h-50px bg-main-blue rounded-6px">
-        내 차 만들기
+        {MAKING_MY_CAR_TITLE}
       </li>
       <li className="flex items-center justify-center text-white w-140px h-50px bg-main-blue rounded-6px">
-        내 차 만들기
+        {MAKING_MY_CAR_TITLE}
       </li>
       <li className="flex items-center justify-center text-white w-140px h-50px bg-main-blue rounded-6px">
-        내 차 만들기
+        {MAKING_MY_CAR_TITLE}
       </li>
       <li className="flex items-center justify-center text-white w-140px h-50px bg-main-blue rounded-6px">
-        내 차 만들기
+        {MAKING_MY_CAR_TITLE}
       </li>
     </OptionLayout>
   );
