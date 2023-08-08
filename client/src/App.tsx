d642363b76b3323b8d1d27d94ece79e3b0108acc
bae-sh <b5460881@naver.com>
2023-08-08 16:21:23 +0900

    feat: 라우터 기능 구현

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,11 @@
+import { Outlet } from 'react-router-dom';
+
 function App() {
   return (
     <>
       <div className=" title1">title-1</div>
       <div className="head-title3-regular">title-2</div>
+      <Outlet />
     </>
   );
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { RouterProvider } from 'react-router-dom';
+import router from './router/index.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
