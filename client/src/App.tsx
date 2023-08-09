5120cdf9cfa4ba2a818d943388654795116414ce
bae-sh <b5460881@naver.com>
2023-08-09 16:51:52 +0900

    chore: App 컴포넌트의 Home 컴포넌트 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
+import Home from './pages/home';
 
 function App() {
   return (
     <>
       <Header />
-      <div className=" title1">title-1</div>
-      <div className="head-title3-regular">title-2</div>
+      <Home />
       <Outlet />
     </>
   );
