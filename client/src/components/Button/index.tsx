5fea28c411950d7911657795f696b1243193cee2
bae-sh <b5460881@naver.com>
2023-08-09 18:40:01 +0900

    refactor: 버튼 사이즈, 색상 추출 함수를 객체로 변경

--- client/src/components/Button/index.tsx
+++ client/src/components/Button/index.tsx
@@ -1,48 +1,38 @@
 type ColorType = 'main-blue' | 'white' | 'grey';
 interface ButtonProps {
   children: React.ReactNode;
-  title: string;
-  size: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
-  color: ColorType;
-  onClick: () => void;
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+  color?: ColorType;
+  onClick?: () => void;
 }
 
-function Button({ children, size, color, onClick }: ButtonProps) {
-  const buttonColor = getButtonColor(color);
-  const buttonSize = getButtonSize(size);
-  const className = `flex justify-center items-center rounded-[6px] ${buttonSize} ${buttonColor}`;
+const buttonSize = {
+  xs: 'w-80px h-32px',
+  sm: 'w-113px h-46px',
+  md: 'w-140px h-50px',
+  lg: 'w-150px h-46px',
+  xl: 'w-244px h-57px',
+};
+
+const buttonColor = {
+  'main-blue': 'bg-main-blue text-white',
+  'white': 'bg-white text-black border border-black',
+  'grey': 'bg-grey-001 text-main-blue',
+};
+
+function Button({
+  children,
+  size = 'md',
+  color = 'main-blue',
+  onClick,
+}: ButtonProps) {
+  const className = `flex justify-center items-center rounded-[6px] ${buttonSize[size]} ${buttonColor[color]}`;
 
   return (
     <button className={className} onClick={onClick}>
       {children}
     </button>
   );
-}
-
-function getButtonSize(size: string) {
-  switch (size) {
-    case 'xs':
-      return 'w-80px h-32px';
-    case 'sm':
-      return 'w-113px h-46px';
-    case 'md':
-      return 'w-140px h-50px';
-    case 'lg':
-      return 'w-150px h-46px';
-    case 'xl':
-      return 'w-244px h-57px';
-  }
-}
-
-function getButtonColor(color: string) {
-  switch (color) {
-    case 'main-blue':
-      return 'bg-main-blue text-white';
-    case 'white':
-      return 'bg-white text-black border border-black';
-    case 'grey':
-      return 'bg-grey-001 text-main-blue';
-  }
 }
 
 export default Button;
