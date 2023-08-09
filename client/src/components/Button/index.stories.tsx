8baefe65e9aef3371d25f8e912f834ce20830037
bae-sh <b5460881@naver.com>
2023-08-09 11:06:14 +0900

    feat: 버튼 공통 컴포넌트 구현

--- client/src/components/Button/index.stories.tsx
+++ client/src/components/Button/index.stories.tsx
@@ -3,12 +3,27 @@ import Button from './index';
 
 const meta: Meta<typeof Button> = {
   component: Button,
+  argTypes: { onClick: { action: 'clicked' } },
 };
 
 export default meta;
 
 type Story = StoryObj<typeof Button>;
 
-export const FirstStory: Story = {
-  args: {},
+export const StorySize: Story = {
+  args: {
+    children: 'StorySize',
+    size: 'sm',
+    color: 'main-blue',
+    onClick: () => {},
+  },
+};
+
+export const StoryColor: Story = {
+  args: {
+    children: 'StoryColor',
+    size: 'sm',
+    color: 'grey',
+    onClick: () => {},
+  },
 };
--- client/src/components/Button/index.tsx
+++ client/src/components/Button/index.tsx
@@ -1,11 +1,48 @@
-import { ReactNode } from 'react';
-
+type ColorType = 'main-blue' | 'white' | 'grey';
 interface ButtonProps {
-  children: ReactNode;
+  children: React.ReactNode;
+  title: string;
+  size: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+  color: ColorType;
+  onClick: () => void;
 }
 
-function Button({ children }: ButtonProps) {
-  return <button>{children}</button>;
+function Button({ children, size, color, onClick }: ButtonProps) {
+  const buttonColor = getButtonColor(color);
+  const buttonSize = getButtonSize(size);
+  const className = `flex justify-center items-center rounded-[6px] ${buttonSize} ${buttonColor}`;
+
+  return (
+    <button className={className} onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
+function getButtonSize(size: string) {
+  switch (size) {
+    case 'xs':
+      return 'w-[80px] h-[32px]';
+    case 'sm':
+      return 'w-[113px] h-[46px]';
+    case 'md':
+      return 'w-[140px] h-[50px]';
+    case 'lg':
+      return 'w-[150px] h-[46px]';
+    case 'xl':
+      return 'w-[244px] h-[57px]';
+  }
+}
+
+function getButtonColor(color: string) {
+  switch (color) {
+    case 'main-blue':
+      return 'bg-main-blue text-white';
+    case 'white':
+      return 'bg-white text-black border border-black';
+    case 'grey':
+      return 'bg-grey-001 text-main-blue';
+  }
 }
 
 export default Button;
