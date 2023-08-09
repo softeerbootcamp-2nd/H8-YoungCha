804532a201b2251b967f72ac4e45d82149d7f43b
bae-sh <b5460881@naver.com>
2023-08-09 18:38:36 +0900

    chore: 버튼 컴포넌트 스토리북 로직 변경

--- client/src/components/Button/index.stories.tsx
+++ client/src/components/Button/index.stories.tsx
@@ -10,20 +10,47 @@ export default meta;
 
 type Story = StoryObj<typeof Button>;
 
-export const StorySize: Story = {
+export const Default: Story = {
   args: {
-    children: 'StorySize',
-    size: 'sm',
+    children: 'Default',
+    size: 'md',
     color: 'main-blue',
     onClick: () => {},
   },
 };
-
-export const StoryColor: Story = {
-  args: {
-    children: 'StoryColor',
-    size: 'sm',
-    color: 'grey',
-    onClick: () => {},
-  },
+export const Colors: Story = {
+  render: () => (
+    <>
+      <Button size="md" color="main-blue">
+        main-blue
+      </Button>
+      <Button size="md" color="white">
+        white
+      </Button>
+      <Button size="md" color="grey">
+        grey
+      </Button>
+    </>
+  ),
+};
+export const Sizes: Story = {
+  render: () => (
+    <>
+      <Button size="xl" color="main-blue">
+        xl
+      </Button>
+      <Button size="lg" color="main-blue">
+        lg
+      </Button>
+      <Button size="md" color="main-blue">
+        md
+      </Button>
+      <Button size="sm" color="main-blue">
+        sm
+      </Button>
+      <Button size="xs" color="main-blue">
+        xs
+      </Button>
+    </>
+  ),
 };
