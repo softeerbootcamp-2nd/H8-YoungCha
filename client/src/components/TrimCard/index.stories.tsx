68370e9b2eb9f66a1b2290a85c9afed379bdd909
1lsang <ilsangpark.98@gmail.com>
2023-08-09 10:49:35 +0900

    feat: Trim Card 컴포넌트 구현

--- client/src/components/TrimCard/index.stories.tsx
+++ client/src/components/TrimCard/index.stories.tsx
@@ -1,14 +1,32 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import TrimCard from './index';
+import { withRouter } from 'storybook-addon-react-router-v6';
 
 const meta: Meta<typeof TrimCard> = {
   component: TrimCard,
+  argTypes: {},
+  parameters: {
+    layout: 'centered',
+  },
+  decorators: [withRouter],
 };
 
 export default meta;
 
 type Story = StoryObj<typeof TrimCard>;
 
-export const FirstStory: Story = {
-  args: {},
+export const Default: Story = {
+  args: {
+    name: 'LeBlanc',
+    hashtag: '#베스트셀러',
+    minPrice: 10000,
+  },
+};
+
+export const GuideMode: Story = {
+  args: {
+    name: 'Guide Mode',
+    hashtag: '#나만을 위한 팰리세이드',
+    minPrice: 38000000,
+  },
 };
--- client/src/components/TrimCard/index.tsx
+++ client/src/components/TrimCard/index.tsx
@@ -1,9 +1,30 @@
+import ChevronRight from '@/assets/icons/ChevronRight';
+import { Link } from 'react-router-dom';
+
+// TYPES
 interface TrimCardProps {
-  children: React.ReactNode;
+  name: string;
+  hashtag: string;
+  minPrice: number;
 }
 
-function TrimCard({ children }: TrimCardProps) {
-  return <div>{children}</div>;
+function TrimCard({ name = '', hashtag = '#', minPrice = 0 }: TrimCardProps) {
+  return (
+    <div className="relative">
+      <Link to={'/making/self/1'}>
+        <div className="body3 bg-white rounded-6px p-20px w-192px h-123px text=grey-black hover:bg-main-blue hover:text-white transition-colors duration-200 cursor-pointer">
+          <div className="mb-8px ">{hashtag}</div>
+          <div className="title2 mb-16px">{name}</div>
+          <div className="flex items-center justify-between">
+            <div className="opacity-80">
+              {minPrice.toLocaleString()} 원 부터
+            </div>
+            <ChevronRight />
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
 }
 
 export default TrimCard;
