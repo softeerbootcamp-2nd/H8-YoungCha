fba39ab205529f9c55d72c16382de5c9180ec18a
HyeonSeongKang <gt9697@naver.com>
2023-08-10 21:30:36 +0900

    feat: 내 차 만들기 데이터 클래스 기반 TabLayout 생성

--- android/app/src/main/java/com/youngcha/ohmycarset/enums/CarOption.kt
+++ android/app/src/main/java/com/youngcha/ohmycarset/enums/CarOption.kt
@@ -1,11 +1,11 @@
 package com.youngcha.ohmycarset.enums
 
-enum class CarOption {
-    POWER_TRAIN,
-    DRIVE_TYPE,
-    BODY_TYPE,
-    EXTERIOR_COLOR,
-    INTERIOR_COLOR,
-    WHEEL_SELECTION,
-    OPTION_SELECTION,
+enum class CarOption(val displayName: String) {
+    POWER_TRAIN("파워트레인"),
+    DRIVE_TYPE("구동 방식"),
+    BODY_TYPE("바디 타입"),
+    EXTERIOR_COLOR("외장 색상"),
+    INTERIOR_COLOR("내장 색상"),
+    WHEEL_SELECTION("휠 선택"),
+    OPTION_SELECTION("옵션 선택"),
 }
