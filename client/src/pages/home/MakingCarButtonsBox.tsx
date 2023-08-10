960b7d2e72f58c7ea700462151b364ebbc1b2c7c
bae-sh <b5460881@naver.com>
2023-08-10 11:49:40 +0900

    feat: 내차만들기 공통 컴포넌트 및 라우팅 적용

--- client/src/pages/home/MakingCarButtonsBox.tsx
+++ client/src/pages/home/MakingCarButtonsBox.tsx
@@ -1,22 +1,19 @@
+import Button from '@/components/Button';
 import OptionLayout from './OptionLayout';
+import { Link } from 'react-router-dom';
 
 const MAKING_MY_CAR_TITLE = '내 차 만들기';
 
 function MakingCarButtonsBox() {
   return (
     <OptionLayout>
-      <li className="flex items-center justify-center text-white w-140px h-50px bg-main-blue rounded-6px">
-        {MAKING_MY_CAR_TITLE}
-      </li>
-      <li className="flex items-center justify-center text-white w-140px h-50px bg-main-blue rounded-6px">
-        {MAKING_MY_CAR_TITLE}
-      </li>
-      <li className="flex items-center justify-center text-white w-140px h-50px bg-main-blue rounded-6px">
-        {MAKING_MY_CAR_TITLE}
-      </li>
-      <li className="flex items-center justify-center text-white w-140px h-50px bg-main-blue rounded-6px">
-        {MAKING_MY_CAR_TITLE}
-      </li>
+      {Array.from({ length: 4 }).map((_, index) => (
+        <Link to="/model/LX06/making/self/1" key={index}>
+          <Button size="md" color="main-blue">
+            {MAKING_MY_CAR_TITLE}
+          </Button>
+        </Link>
+      ))}
     </OptionLayout>
   );
 }
