1d17ff9958e722ee193757926aec0b8303fa427c
bae-sh <b5460881@naver.com>
2023-08-10 19:19:10 +0900

    chore: 스크롤시 헤더 배경 변경

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,17 +1,24 @@
 import HyundaiLogoButton from './HyundaiLogoButton';
 import DictionaryToggleButton from './DictionaryToggleButton';
 import ModelChangeButton from './ModelChangeButton';
-
-interface HeaderProps {
-  backgroundColor?: string;
-}
+import { useEffect, useState } from 'react';
 
 const TEXT_MAKING_MY_CAR = '내 차 만들기';
 
-function Header({ backgroundColor }: HeaderProps) {
+function Header() {
+  const [backgroundColor, setBackgroundColor] = useState('');
+  useEffect(() => {
+    document.addEventListener('scroll', () => {
+      if (window.scrollY === 0) setBackgroundColor('');
+      if (window.scrollY > 0) setBackgroundColor('bg-grey-001');
+    });
+    return () => {
+      document.removeEventListener('scroll', () => {});
+    };
+  }, []);
   return (
     <div
-      className={`flex justify-between max-w-7xl px-128px py-32px fixed z-100 w-full mx-auto left-0 right-0 ${backgroundColor}`}
+      className={`flex justify-between  px-128px py-32px fixed z-100 w-full mx-auto left-0 right-0 ${backgroundColor} duration-500 z-10`}
     >
       <div className="flex text-center">
         <HyundaiLogoButton />
