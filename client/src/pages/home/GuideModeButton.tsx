d4b1cfa41dd17bf7a517960148948fe6fcd88009
bae-sh <b5460881@naver.com>
2023-08-10 11:50:43 +0900

    feat: 가이드 모드 버튼 라우팅 추가

--- client/src/pages/home/GuideModeButton.tsx
+++ client/src/pages/home/GuideModeButton.tsx
@@ -1,15 +1,19 @@
+import { Link } from 'react-router-dom';
+
 function GuideModeButton() {
   return (
-    <div className="flex justify-center">
-      <div className="flex items-center justify-center gap-80px bg-grey-001 w-533px h-90px rounded-6px">
-        <p className="title5 text-grey-black">
-          무엇을 골라야 할 지 모르겠다면?
-        </p>
-        <p className="text-32px font-hsans-head text-grey-black font-medium tracking-[-0.96px]">
-          Guide Mode
-        </p>
+    <Link to="/model/LX06/guide/age">
+      <div className="flex justify-center">
+        <div className="flex items-center justify-center gap-80px bg-grey-001 w-533px h-90px rounded-6px">
+          <p className="title5 text-grey-black">
+            무엇을 골라야 할 지 모르겠다면?
+          </p>
+          <p className="text-32px font-hsans-head text-grey-black font-medium tracking-[-0.96px]">
+            Guide Mode
+          </p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
