b2caf2b4ad215d7e1013f7f2f2f88ed27f7ecbc5
HyeonSeongKang <gt9697@naver.com>
2023-08-10 20:26:48 +0900

    refactor: 각 옵션 대표 이미지 추가할 수 있도록 구조 변경

--- android/app/src/main/java/com/youngcha/ohmycarset/model/car/Car.kt
+++ android/app/src/main/java/com/youngcha/ohmycarset/model/car/Car.kt
@@ -7,5 +7,7 @@ data class Car(
     val name: String, // 자동차 이름 ex: 팰리세이드
     val type: String, // 자동차 타입 ex: SUV
     val mainOptions: List<Map<CarOption, List<OptionInfo>>>, // 자동차 옵션 ex: key: 파워트레인 value: {디젤, 가솔린}
-    val subOptions: List<Map<OptionSelection, List<OptionInfo>>> // 선택 옵션 부분
+    val mainOptionImages: List<Map<CarOption, Int>>, // 자동차 옵션 이미지
+    val subOptions: List<Map<OptionSelection, List<OptionInfo>>>, // 선택 옵션 부분
+    val subOptionImage: Map<CarOption, Int>
 )
