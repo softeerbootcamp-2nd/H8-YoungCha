01f4b9364a9e0316fb5280d45370c760089bae7c
dohyeon-han <limited.hyeon@gmail.com>
2023-08-10 12:41:23 +0900

    feat: option detail로 변경

--- server/src/main/java/team/youngcha/domain/option/repository/OptionDetailRepository.java
+++ server/src/main/java/team/youngcha/domain/option/repository/OptionDetailRepository.java
@@ -2,8 +2,8 @@ package team.youngcha.domain.option.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import team.youngcha.domain.option.entity.Detail;
+import team.youngcha.domain.option.entity.OptionDetail;
 
 @Repository
-public interface DetailRepository extends CrudRepository<Detail, Long> {
+public interface OptionDetailRepository extends CrudRepository<OptionDetail, Long> {
 }
