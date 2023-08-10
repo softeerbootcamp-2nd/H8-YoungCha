06fe3b41f008133738981054161f7fb5a0b2cd41
dohyeon-han <limited.hyeon@gmail.com>
2023-08-11 00:15:38 +0900

    remove: powerTrain 패키지 삭제

--- server/src/main/java/team/youngcha/domain/powerTrain/repository/PowerTrainRepository.java
+++
@@ -1,7 +0,0 @@
-package team.youngcha.domain.powerTrain.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import team.youngcha.domain.powerTrain.entity.PowerTrain;
-
-public interface PowerTrainRepository extends CrudRepository<PowerTrain, Long> {
-}
