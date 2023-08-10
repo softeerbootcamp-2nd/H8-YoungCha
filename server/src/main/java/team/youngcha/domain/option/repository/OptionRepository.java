f2f2aaf25ad36c7d759c826d266f14d36ad92ab6
dohyeon-han <limited.hyeon@gmail.com>
2023-08-10 22:42:54 +0900

    feat: power train option 조회 메소드 구현

--- server/src/main/java/team/youngcha/domain/option/repository/OptionRepository.java
+++ server/src/main/java/team/youngcha/domain/option/repository/OptionRepository.java
@@ -1,15 +1,45 @@
 package team.youngcha.domain.option.repository;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.stereotype.Repository;
 import team.youngcha.domain.option.entity.Option;
+import team.youngcha.domain.option.entity.OptionType;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class OptionRepository {
+
     private final JdbcTemplate jdbcTemplate;
-    private final RowMapper<Option> optionRowMapper = BeanPropertyRowMapper.newInstance(Option.class);
+    private final RowMapper<Option> optionRowMapper = new OptionRowMapper();
+
+    public List<Option> findPowerTrainsByTrimIdAndType(Long trimId, OptionType type) {
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("trim_id", trimId);
+        params.addValue("type", type.getType());
+
+        return jdbcTemplate.query("select * from options " +
+                        "join category on options.category_id = category.id and category.name = '파워 트레인' " +
+                        "join trim_options on options.id = trim_options.options_id " +
+                        "and trim_options.trim_id = (:trimId) and trim_options.type = (:type) ",
+                optionRowMapper, params);
+    }
+
+    private static class OptionRowMapper implements RowMapper<Option> {
+        @Override
+        public Option mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+            return new Option(resultSet.getLong("id"),
+                    resultSet.getString("name"),
+                    resultSet.getInt("price"),
+                    resultSet.getString("feedback"),
+                    resultSet.getLong("category_id")
+            );
+        }
+    }
 }
