2025c057f16cc84fd983494e8acb38c156163d4a
dohyeon-han <limited.hyeon@gmail.com>
2023-08-12 16:37:02 +0900

    chore: keyword id 변수명 변경
    숫자가 앞에 있는 것이 나아보인다.

--- server/src/main/java/team/youngcha/domain/estimate/entity/Estimate.java
+++ server/src/main/java/team/youngcha/domain/estimate/entity/Estimate.java
@@ -19,15 +19,15 @@ public class Estimate {
     private Long exteriorColorId;
     private Long interiorColorId;
     private Long wheelId;
-    private Long keywordId1;
-    private Long keywordId2;
-    private Long keywordId3;
+    private Long keyword1Id;
+    private Long keyword2Id;
+    private Long keyword3Id;
     private int ageRange;
     private int gender;
     private LocalDateTime createDate;
 
     @Builder
-    public Estimate(Long id, Long trimId, Long engineId, Long bodyTypeId, Long drivingSystemId, Long exteriorColorId, Long interiorColorId, Long wheelId, Long keywordId1, Long keywordId2, Long keywordId3, int ageRange, int gender) {
+    public Estimate(Long id, Long trimId, Long engineId, Long bodyTypeId, Long drivingSystemId, Long exteriorColorId, Long interiorColorId, Long wheelId, Long keyword1Id, Long keyword2Id, Long keyword3Id, int ageRange, int gender) {
         this.id = id;
         this.trimId = trimId;
         this.engineId = engineId;
@@ -36,9 +36,9 @@ public class Estimate {
         this.exteriorColorId = exteriorColorId;
         this.interiorColorId = interiorColorId;
         this.wheelId = wheelId;
-        this.keywordId1 = keywordId1;
-        this.keywordId2 = keywordId2;
-        this.keywordId3 = keywordId3;
+        this.keyword1Id = keyword1Id;
+        this.keyword2Id = keyword2Id;
+        this.keyword3Id = keyword3Id;
         this.ageRange = ageRange;
         this.gender = gender;
         this.createDate = LocalDateTime.now();
--- server/src/main/java/team/youngcha/domain/estimate/repository/EstimateRepository.java
+++ server/src/main/java/team/youngcha/domain/estimate/repository/EstimateRepository.java
@@ -4,13 +4,64 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 import team.youngcha.domain.estimate.entity.Estimate;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
 public class EstimateRepository {
 
-    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
     private final RowMapper<Estimate> estimateRowMapper = BeanPropertyRowMapper.newInstance(Estimate.class);
+
+    public Map<Long, Long> countPowerTrainByTrimIdAndContainPowerTrainIds(Long trimId, List<Long> powerTrainIds) {
+        List<Map<String, Object>> results = querySellCounts(namedParameterJdbcTemplate, trimId, powerTrainIds);
+
+        return results.stream()
+                .collect(Collectors.toMap(
+                        row -> (Long) row.get("engine_id"),
+                        row -> (Long) row.get("count")
+                ));
+    }
+
+    private List<Map<String, Object>> querySellCounts(NamedParameterJdbcTemplate namedParameterJdbcTemplate, Long trimId, List<Long> powerTrainIds) {
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("trimId", trimId);
+        params.addValue("powerTrainIds", powerTrainIds);
+
+        return namedParameterJdbcTemplate.queryForList(
+                "select sell.engine_id, COUNT(*) as count from sell " +
+                        "where sell.engine_id in (:powerTrainIds) and sell.trim_id in (:trimId)" +
+                        "group by sell.engine_id",
+                params);
+    }
+
+    private static class EstimateRowMapper implements RowMapper<Estimate> {
+        @Override
+        public Estimate mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+            return Estimate.builder()
+                    .id(resultSet.getLong("id"))
+                    .trimId(resultSet.getLong("trim_id"))
+                    .engineId(resultSet.getLong("engine_id"))
+                    .bodyTypeId(resultSet.getLong("body_type_id"))
+                    .drivingSystemId(resultSet.getLong("driving_system_id"))
+                    .exteriorColorId(resultSet.getLong("exterior_color_id"))
+                    .interiorColorId(resultSet.getLong("interior_color_id"))
+                    .wheelId(resultSet.getLong("wheel_id"))
+                    .keyword1Id(resultSet.getLong("keyword1_id"))
+                    .keyword2Id(resultSet.getLong("keyword2_id"))
+                    .keyword3Id(resultSet.getLong("keyword3_id"))
+                    .ageRange(resultSet.getInt("age"))
+                    .gender(resultSet.getInt("gender"))
+                    .build();
+        }
+    }
 }
