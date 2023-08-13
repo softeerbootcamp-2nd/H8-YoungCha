a00c7f7432dd5c0a75387b9035d6b60d105af727
dohyeon-han <limited.hyeon@gmail.com>
2023-08-13 21:06:45 +0900

    feat: GuideInfo 객체 생성

---
+++ server/src/main/java/team/youngcha/domain/option/dto/GuideInfo.java
@@ -0,0 +1,26 @@
+package team.youngcha.domain.option.dto;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import team.youngcha.common.type.AgeRange;
+import team.youngcha.common.type.Gender;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GuideInfo {
+
+    private Gender gender;
+    private AgeRange ageRange;
+    private List<Long> keywordIds;
+
+    @Builder
+    public GuideInfo(Gender gender, AgeRange ageRange, List<Long> keywordIds) {
+        this.gender = gender;
+        this.ageRange = ageRange;
+        this.keywordIds = keywordIds;
+    }
+}
