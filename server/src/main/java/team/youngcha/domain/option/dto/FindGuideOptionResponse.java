dcc137aef9f68715aacc2a108efb4a3f5864a966
dohyeon-han <limited.hyeon@gmail.com>
2023-08-13 22:41:16 +0900

    feat: 파워트레인 가이드 모드 조회 service 로직 구현

---
+++ server/src/main/java/team/youngcha/domain/option/dto/FindGuideOptionResponse.java
@@ -0,0 +1,53 @@
+package team.youngcha.domain.option.dto;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import team.youngcha.domain.keyword.dto.KeywordRate;
+import team.youngcha.domain.option.entity.Option;
+import team.youngcha.domain.option.entity.OptionDetail;
+import team.youngcha.domain.option.entity.OptionImage;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class FindGuideOptionResponse {
+
+    private Long id;
+    private boolean isChecked;
+    private int rate;
+    private int price;
+    private String name;
+    private List<KeywordRate> tags;
+    private List<FindOptionDetailResponse> details;
+    private List<FindOptionImageResponse> images;
+
+    public FindGuideOptionResponse(Option option, boolean isChecked, int rate, List<KeywordRate> tags, List<OptionImage> optionImages, List<OptionDetail> optionDetails) {
+        this.id = option.getId();
+        this.isChecked = isChecked;
+        this.rate = rate;
+        this.price = option.getPrice();
+        this.name = option.getName();
+        this.tags = tags;
+        this.images = optionImages.stream()
+                .map(FindOptionImageResponse::new)
+                .collect(Collectors.toList());
+        this.details = optionDetails.stream()
+                .map(FindOptionDetailResponse::new)
+                .collect(Collectors.toList());
+    }
+
+    @Builder
+    public FindGuideOptionResponse(boolean isChecked, int rate, String name, List<KeywordRate> tags,
+                                   List<FindOptionDetailResponse> details, List<FindOptionImageResponse> images) {
+        this.isChecked = isChecked;
+        this.rate = rate;
+        this.name = name;
+        this.tags = tags;
+        this.details = details;
+        this.images = images;
+    }
+}
--- server/src/main/java/team/youngcha/domain/option/service/OptionService.java
+++ server/src/main/java/team/youngcha/domain/option/service/OptionService.java
@@ -4,7 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import team.youngcha.common.exception.CustomException;
+import team.youngcha.domain.estimate.repository.EstimateRepository;
+import team.youngcha.domain.keyword.dto.KeywordRate;
+import team.youngcha.domain.keyword.entity.Keyword;
+import team.youngcha.domain.keyword.repository.KeywordRepository;
+import team.youngcha.domain.option.dto.FindGuideOptionResponse;
 import team.youngcha.domain.option.dto.FindSelfOptionResponse;
+import team.youngcha.domain.option.dto.GuideInfo;
 import team.youngcha.domain.option.entity.Option;
 import team.youngcha.domain.option.entity.OptionDetail;
 import team.youngcha.domain.option.entity.OptionImage;
@@ -25,11 +31,14 @@ public class OptionService {
     private final TrimRepository trimRepository;
     private final SellRepository sellRepository;
     private final OptionRepository optionRepository;
+    private final KeywordRepository keywordRepository;
+    private final EstimateRepository estimateRepository;
     private final OptionImageRepository optionImageRepository;
     private final OptionDetailRepository optionDetailRepository;
 
     public List<FindSelfOptionResponse> findSelfPowerTrains(Long trimId) {
-        trimRepository.findById(trimId).orElseThrow(() -> new CustomException(HttpStatus.NOT_FOUND, "존재하지 않는 트림입니다."));
+        trimRepository.findById(trimId)
+                .orElseThrow(() -> new CustomException(HttpStatus.NOT_FOUND, "존재하지 않는 트림입니다."));
         List<Option> powerTrains = optionRepository.findPowerTrainsByTrimIdAndType(trimId, OptionType.OPTIONAL);
 
         List<Long> powerTrainIds = powerTrains.stream().map(Option::getId).collect(Collectors.toList());
@@ -40,9 +49,66 @@ public class OptionService {
         return getSortedSelfOptionResponses(powerTrains, sellRatio, powerTrainImagesGroup, powerTrainDetailsGroup);
     }
 
+    public List<FindGuideOptionResponse> findGuidePowerTrains(Long trimId, GuideInfo guideInfo) {
+        trimRepository.findById(trimId)
+                .orElseThrow(() -> new CustomException(HttpStatus.NOT_FOUND, "존재하지 않는 트림입니다."));
+        List<Option> powerTrains = optionRepository.findPowerTrainsByTrimIdAndType(trimId, OptionType.OPTIONAL);
+
+        List<Long> powerTrainIds = powerTrains.stream().map(Option::getId).collect(Collectors.toList());
+
+        Map<Long, List<OptionImage>> powerTrainImagesGroup = getOptionImagesGroup(powerTrainIds);
+        Map<Long, List<OptionDetail>> powerTrainDetailsGroup = getOptionDetailGroup(powerTrainIds);
+
+        // 트림에 해당하는 파워 트레인 별 유사 사용자 수 비율
+        Map<Long, Integer> similarityUsersRatio = getSimilarityUsersRatio(trimId, powerTrainIds, guideInfo);
+
+        // 옵션 별 키워드 조회
+        Map<Long, List<Keyword>> powerTrainIdKeywordGroup = keywordRepository
+                .findByContainOptionIdsAndGroupKeywords(powerTrainIds);
+
+        for (Long keywordId : guideInfo.getKeywordIds()) {
+            // 사용자 키워드 우선순위 순으로 옵션이 해당 키워드를 갖는지 확인
+            List<Long> optionIds = new ArrayList<>();
+            for (Map.Entry<Long, List<Keyword>> entry : powerTrainIdKeywordGroup.entrySet()) {
+                // 옵션의 키워드에 사용자가 선택한 키워드가 있는 판별
+                boolean containId = entry.getValue().stream()
+                        .anyMatch(keyword -> keyword.getId().equals(keywordId));
+                if (!containId) continue;
+                optionIds.add(entry.getKey());
+            }
+            if (optionIds.size() != 1) {
+                continue;
+            }
+            // 겹치는 키워드가 없으면 비율을 조회 후 반환
+            Integer rate = estimateRepository.calculateRate(trimId, optionIds.get(0), keywordId);
+            Keyword keyword = keywordRepository.findById(keywordId)
+                    .orElseThrow(() -> new CustomException(HttpStatus.NOT_FOUND, "키워드를 찾을 수 없습니다."));
+            KeywordRate keywordRate = new KeywordRate(rate, keyword.getName());
+            Map<Long, List<KeywordRate>> keywordRateGroup = new HashMap<>();
+            keywordRateGroup.put(optionIds.get(0), List.of(keywordRate));
+            return getSortedGuideOptionResponses(powerTrains, similarityUsersRatio, keywordRateGroup,
+                    powerTrainImagesGroup, powerTrainDetailsGroup);
+        }
+        // 모두 키워드가 없으면 유사 사용자 비율
+        return getSortedGuideOptionResponses(powerTrains, similarityUsersRatio, new HashMap<>(),
+                powerTrainImagesGroup, powerTrainDetailsGroup);
+    }
+
     private Map<Long, Integer> getSellRatio(Long trimId, List<Long> optionsIds) {
-        Map<Long, Long> powerTrainCounts = sellRepository.countPowerTrainByTrimIdAndContainPowerTrainIds(trimId, optionsIds);
+        Map<Long, Long> powerTrainCounts = sellRepository
+                .countPowerTrainByTrimIdAndContainPowerTrainIds(trimId, optionsIds);
         addMissingOptionIds(powerTrainCounts, optionsIds);
+        return calculateOptionRatios(powerTrainCounts);
+    }
+
+    private Map<Long, Integer> getSimilarityUsersRatio(Long trimId, List<Long> optionsIds, GuideInfo guideInfo) {
+        Map<Long, Long> powerTrainCounts = estimateRepository
+                .countPowerTrainsSimilarityUsers(trimId, optionsIds, guideInfo);
+        addMissingOptionIds(powerTrainCounts, optionsIds);
+        return calculateOptionRatios(powerTrainCounts);
+    }
+
+    private Map<Long, Integer> calculateOptionRatios(Map<Long, Long> powerTrainCounts) {
         long total = powerTrainCounts.values().stream().mapToLong(Long::longValue).sum();
 
         Map<Long, Integer> optionRatios = new HashMap<>();
@@ -80,6 +146,50 @@ public class OptionService {
                         powerTrainImagesGroup.getOrDefault(powerTrain.getId(), new ArrayList<>()),
                         powerTrainDetailsGroup.getOrDefault(powerTrain.getId(), new ArrayList<>())))
                 .sorted(Comparator.comparingDouble(response -> -response.getRate()))
+                .collect(Collectors.toList());
+    }
+
+    private List<FindGuideOptionResponse> getSortedGuideOptionResponses(List<Option> powerTrains,
+                                                                        Map<Long, Integer> similarityUsersRatio,
+                                                                        Map<Long, List<KeywordRate>> keywordRateGroup,
+                                                                        Map<Long, List<OptionImage>> powerTrainImagesGroup,
+                                                                        Map<Long, List<OptionDetail>> powerTrainDetailsGroup) {
+        if (keywordRateGroup.isEmpty()) {
+            return powerTrains.stream()
+                    .map(powerTrain -> {
+                        Integer ratio = similarityUsersRatio.get(powerTrain.getId());
+                        boolean isMaxRatio = ratio.equals(Collections.max(similarityUsersRatio.values()));
+                        if (isMaxRatio) { // 최대값이 하나만 되도록 해서 isChecked가 여러개 되는 것을 방지
+                            similarityUsersRatio.put(powerTrain.getId(), Integer.MAX_VALUE);
+                        }
+                        return new FindGuideOptionResponse(
+                                powerTrain,
+                                isMaxRatio,
+                                ratio,
+                                keywordRateGroup.getOrDefault(powerTrain.getId(), new ArrayList<>()),
+                                powerTrainImagesGroup.getOrDefault(powerTrain.getId(), new ArrayList<>()),
+                                powerTrainDetailsGroup.getOrDefault(powerTrain.getId(), new ArrayList<>())
+                        );
+                    })
+                    .sorted(Comparator.comparingDouble((FindGuideOptionResponse response) -> -response.getRate()) // 비율 내림차순
+                            .thenComparing((FindGuideOptionResponse response) -> response.isChecked() ? 0 : 1)) // true가 맨 처음
+                    .collect(Collectors.toList());
+        }
+        return powerTrains.stream()
+                .map(powerTrain -> {
+                    Integer ratio = similarityUsersRatio.get(powerTrain.getId());
+                    boolean isSelected = keywordRateGroup.getOrDefault(powerTrain.getId(), new ArrayList<>()).isEmpty();
+                    return new FindGuideOptionResponse(
+                            powerTrain,
+                            isSelected,
+                            ratio,
+                            keywordRateGroup.getOrDefault(powerTrain.getId(), new ArrayList<>()),
+                            powerTrainImagesGroup.getOrDefault(powerTrain.getId(), new ArrayList<>()),
+                            powerTrainDetailsGroup.getOrDefault(powerTrain.getId(), new ArrayList<>())
+                    );
+                })
+                .sorted(Comparator.comparingDouble((FindGuideOptionResponse response) -> -response.getRate()) // 비율 내림차순
+                        .thenComparing((FindGuideOptionResponse response) -> response.isChecked() ? 0 : 1)) // true가 맨 처음
                 .collect(Collectors.toList());
     }
 }
