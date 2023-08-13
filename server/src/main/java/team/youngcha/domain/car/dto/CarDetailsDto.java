82aea98b6a91d12d4e211833f72d84e82c4542af
csct3434 <csct3434@gmail.com>
2023-08-13 21:01:45 +0900

    test: TrimService.extractTrimDetailsFromCarDetailsDtos 테스트 작성

--- server/src/main/java/team/youngcha/domain/car/dto/CarDetailsDto.java
+++ server/src/main/java/team/youngcha/domain/car/dto/CarDetailsDto.java
@@ -23,4 +23,21 @@ public class CarDetailsDto {
     private String optionCategoryName;
     private Integer optionImgType;
     private String optionImgUrl;
+
+    public CarDetailsDto(Long carId, String carName, Long trimId, String trimName, String trimImgUrl, String trimBackgroundImgUrl, String trimHashTag, Integer trimPrice, String trimDescription, Integer trimOptionType, String optionName, String optionCategoryName, Integer optionImgType, String optionImgUrl) {
+        this.carId = carId;
+        this.carName = carName;
+        this.trimId = trimId;
+        this.trimName = trimName;
+        this.trimImgUrl = trimImgUrl;
+        this.trimBackgroundImgUrl = trimBackgroundImgUrl;
+        this.trimHashTag = trimHashTag;
+        this.trimPrice = trimPrice;
+        this.trimDescription = trimDescription;
+        this.trimOptionType = trimOptionType;
+        this.optionName = optionName;
+        this.optionCategoryName = optionCategoryName;
+        this.optionImgType = optionImgType;
+        this.optionImgUrl = optionImgUrl;
+    }
 }
