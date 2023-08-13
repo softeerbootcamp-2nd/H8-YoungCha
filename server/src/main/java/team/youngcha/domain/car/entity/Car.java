6577c66e7bcd4e352a4d8f702211006c91ab5a3f
csct3434 <csct3434@gmail.com>
2023-08-13 15:35:11 +0900

    fix: Car 클래스가 BeanPropertyRowMapper에 의해 초기화되지 않는 오류 해결
    Car 클래스에 기본 생성자와 Setter가 존재하지 않아 BeanPropertyRowMapper에서 초기화되지 못하는 오류 해결

--- server/src/main/java/team/youngcha/domain/car/entity/Car.java
+++ server/src/main/java/team/youngcha/domain/car/entity/Car.java
@@ -1,14 +1,14 @@
 package team.youngcha.domain.car.entity;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
 public class Car {
 
     private Long id;
     private String name;
-
-    public Car(String name) {
-        this.name = name;
-    }
 }
