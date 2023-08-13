64ff8d8c26a258baf026ae7b40442aa2f56a6e32
dohyeon-han <limited.hyeon@gmail.com>
2023-08-13 12:27:08 +0900

    feat: swagger origin 설정

--- server/src/main/java/team/youngcha/common/config/SwaggerConfig.java
+++ server/src/main/java/team/youngcha/common/config/SwaggerConfig.java
@@ -3,15 +3,26 @@ package team.youngcha.common.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${spring.origin}")
+    private String ORIGIN;
+
     @Bean
     public OpenAPI openAPI() {
+        Server server = new Server();
+        server.setUrl(ORIGIN);
         return new OpenAPI()
                 .components(new Components())
+                .servers(List.of(server))
                 .info(apiInfo());
     }
 
