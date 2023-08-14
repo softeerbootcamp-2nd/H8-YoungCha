83cf6e431f0f7ac6493ea7fdc122780d06af834a
csct3434 <csct3434@gmail.com>
2023-08-14 21:35:27 +0900

    feat: 트림 기본 품목 조회 API 메서드 작성

---
+++ server/src/main/java/team/youngcha/domain/trim/controller/TrimController.java
@@ -0,0 +1,29 @@
+package team.youngcha.domain.trim.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import team.youngcha.common.dto.SuccessResponse;
+import team.youngcha.domain.trim.dto.FindTrimDefaultOptionsResponse;
+import team.youngcha.domain.trim.service.TrimService;
+
+@RequestMapping("/trims")
+@RestController
+@RequiredArgsConstructor
+public class TrimController {
+
+    private final TrimService trimService;
+
+    @GetMapping("/{id}/default-components")
+    public ResponseEntity<SuccessResponse<FindTrimDefaultOptionsResponse>> findTrimDefaultOptions(
+            @PathVariable Long id,
+            @RequestParam int categoryId,
+            @RequestParam int page,
+            @RequestParam int size
+    ) {
+        FindTrimDefaultOptionsResponse findTrimDefaultOptionsResponse = trimService.findPaginatedDefaultOptions(id, categoryId, page, size);
+        SuccessResponse<FindTrimDefaultOptionsResponse> successResponse = new SuccessResponse<>(findTrimDefaultOptionsResponse);
+        return ResponseEntity.ok().body(successResponse);
+    }
+
+}
