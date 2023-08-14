285572fd7e6b23878ea1b830cfec348561f64ed3
bae-sh <b5460881@naver.com>
2023-08-14 13:48:13 +0900

    feat: making 페이지 레이아웃 구현

--- client/src/pages/making/index.tsx
+++ client/src/pages/making/index.tsx
@@ -16,7 +16,7 @@ const TITLE = [
   '옵션',
 ];
 
-const PULL_IMAGE_PAGE = [1, 3, 4];
+const PULL_IMAGE_PAGE = [1, 2, 4];
 
 function Making({ path }: MakingProps) {
   const { step, id } = useParams() as { step: string; id: string };
@@ -26,59 +26,53 @@ function Making({ path }: MakingProps) {
     ? 'px-128px'
     : 'pr-128px';
 
-  const imgSize = PULL_IMAGE_PAGE.includes(currentStep)
-    ? 'w-[60vw] h-full top-0 left-0 absolute'
-    : '';
-
   return (
-    <div className="w-full h-screen pt-85px">
-      <ProgressBar />
+    <div className="flex flex-col w-full h-screen pt-85px">
+      <ProgressBar step={currentStep} path={path} id={id} />
       <div
-        className={`m-auto max-w-7xl ${paddingSize} flex justify-between items-center w-full gap-60px`}
+        className={`m-auto max-w-7xl ${paddingSize} flex justify-between items-center w-full flex-1`}
       >
-        <div className="flex items-center justify-center flex-1">
-          <img
-            src="/src/assets/mock/images/palisade.png"
-            alt="palisade"
-            className={imgSize}
-          />
+        <div className="flex items-center justify-center ">
+          <img src="/src/assets/mock/images/palisade.png" alt="palisade" />
         </div>
-        <div className="flex flex-col h-full w-375px pt-72px gap-20px">
+        <div className="flex flex-col justify-center h-full w-375px gap-30px">
           <div className="flex gap-12px">
             <h1 className="font-hsans-head text-24px tracking-[-0.96px]">
               <strong className="font-medium">{TITLE[currentStep - 1]}</strong>
               을 선택해주세요.
             </h1>
           </div>
-          <div className="flex flex-col items-center gap-12px h-350px">
-            <div className="flex items-center justify-center border rounded-lg border-main-blue w-375px h-150px">
-              디젤 2.2 box
-            </div>
-            <div className="flex items-center justify-center border rounded-lg border-main-blue w-375px h-150px">
-              디젤 2.2 box
-            </div>
-          </div>
-
-          <div className="flex justify-between">
-            <div className="flex flex-col gap-5px">
-              <div className="flex gap-5px">
-                <span className="body2 text-grey-003">총 견적금액</span>
-                <button>^</button>
+          <div>
+            <div className="flex flex-col items-center gap-12px h-400px">
+              <div className="flex items-center justify-center border rounded-lg border-main-blue w-375px h-150px">
+                디젤 2.2 box
               </div>
-              <span className="title1 text-grey-black">47,200,000원</span>
+              <div className="flex items-center justify-center border rounded-lg border-main-blue w-375px h-150px">
+                디젤 2.2 box
+              </div>
             </div>
-            <div className="flex items-center gap-21px">
-              {currentStep !== 1 && (
-                <Link
-                  to={`/model/${id}/making/${path}/${Number(step) - 1}`}
-                  className="body2 text-grey-003 "
-                >
-                  이전
+
+            <div className="flex justify-between">
+              <div className="flex flex-col gap-5px">
+                <div className="flex gap-5px">
+                  <span className="body2 text-grey-003">총 견적금액</span>
+                  <button>^</button>
+                </div>
+                <span className="title1 text-grey-black">47,200,000원</span>
+              </div>
+              <div className="flex items-center gap-21px">
+                {currentStep !== 1 && (
+                  <Link
+                    to={`/model/${id}/making/${path}/${Number(step) - 1}`}
+                    className="body2 text-grey-003 "
+                  >
+                    이전
+                  </Link>
+                )}
+                <Link to={`/model/${id}/making/${path}/${Number(step) + 1}`}>
+                  <Button size="sm">선택 완료</Button>
                 </Link>
-              )}
-              <Link to={`/model/${id}/making/${path}/${Number(step) + 1}`}>
-                <Button size="sm">선택 완료</Button>
-              </Link>
+              </div>
             </div>
           </div>
         </div>
