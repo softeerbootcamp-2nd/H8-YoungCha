80e4e9935c8a8a0452d0e066ead756cf251bb1fd
dohyeon-han <limited.hyeon@gmail.com>
2023-08-15 01:09:52 +0900

    feat: 구동 방식, 바디 타입 controller 구현

--- server/src/main/java/team/youngcha/domain/option/controller/OptionController.java
+++ server/src/main/java/team/youngcha/domain/option/controller/OptionController.java
@@ -32,10 +32,32 @@ public class OptionController {
     ) {
         List<FindSelfOptionResponse> findSelfOptionResponses = optionService
                 .findSelfOptions(trimId, SelectiveCategory.POWER_TRAIN);
-        SuccessResponse<List<FindSelfOptionResponse>> successResponse =
-                new SuccessResponse<>(findSelfOptionResponses);
+        SuccessResponse<List<FindSelfOptionResponse>> successResponse = new SuccessResponse<>(findSelfOptionResponses);
         return ResponseEntity.ok(successResponse);
     }
+
+    @Operation(summary = "구동 방식 셀프 모드 옵션 조회", description = "셀프 모드에서 구동 방식의 옵션을 판매량과 함께 조회합니다.")
+    @GetMapping("/self/driving-system")
+    public ResponseEntity<SuccessResponse<List<FindSelfOptionResponse>>> findSelfDrivingSystem(
+            @PathVariable Long trimId
+    ) {
+        List<FindSelfOptionResponse> findSelfOptionResponses = optionService
+                .findSelfOptions(trimId, SelectiveCategory.DRIVING_SYSTEM);
+        SuccessResponse<List<FindSelfOptionResponse>> successResponse = new SuccessResponse<>(findSelfOptionResponses);
+        return ResponseEntity.ok(successResponse);
+    }
+
+    @Operation(summary = "바디 타입 셀프 모드 옵션 조회", description = "셀프 모드에서 바디 타입의 옵션을 판매량과 함께 조회합니다.")
+    @GetMapping("/self/body-type")
+    public ResponseEntity<SuccessResponse<List<FindSelfOptionResponse>>> findSelfBodyType(
+            @PathVariable Long trimId
+    ) {
+        List<FindSelfOptionResponse> findSelfOptionResponses = optionService
+                .findSelfOptions(trimId, SelectiveCategory.BODY_TYPE);
+        SuccessResponse<List<FindSelfOptionResponse>> successResponse = new SuccessResponse<>(findSelfOptionResponses);
+        return ResponseEntity.ok(successResponse);
+    }
+
 
     @Operation(summary = "파워 트레인 가이드 모드 옵션 조회",
             description = "가이드 모드에서 파워 트레인의 옵션을 유사 사용자 선택량과 함께 조회합니다.")
