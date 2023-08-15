720fb3861bebc73dc09fb7b6dbf69d3a8200a943
csct3434 <csct3434@gmail.com>
2023-08-15 19:07:51 +0900

    chore: OptionDefaultComponentsTest 매직 넘버 제거 및 코드 정리

--- server/src/test/java/team/youngcha/domain/option/repository/OptionDefaultComponentsTest.java
+++ server/src/test/java/team/youngcha/domain/option/repository/OptionDefaultComponentsTest.java
@@ -20,15 +20,25 @@ public class OptionDefaultComponentsTest {
     JdbcTemplate jdbcTemplate;
     OptionRepository optionRepository;
 
+    Long trimId = 2L;
+    Long categoryId = 1L;
+    int pageSize = 5;
+    int totalPages = 2;
+
+    List<DefaultOptionSummary> expectedDefaultOptionSummaries = new ArrayList<>(Arrays.asList(
+            new DefaultOptionSummary("8단 자동변속기", categoryId, "https://www.hyundai.com/contents/spec/LX24/8at_s.jpg"),
+            new DefaultOptionSummary("ISG 시스템", categoryId, "https://www.hyundai.com/contents/spec/LX24/isg_s.jpg"),
+            new DefaultOptionSummary("통합주행모드", categoryId, "https://www.hyundai.com/contents/spec/LX24/drivemode_s.jpg"),
+            new DefaultOptionSummary("랙구동형 전동식 파워스티어링(R-MDPS)", categoryId, "https://www.hyundai.com/contents/spec/LX24/rmdps_s.jpg"),
+            new DefaultOptionSummary("전자식 변속버튼", categoryId, "https://www.hyundai.com/contents/spec/LX24/sbw_s.jpg"),
+            new DefaultOptionSummary("HTARC", categoryId, "https://www.hyundai.com/contents/spec/LX24/htrac_s.jpg"),
+            new DefaultOptionSummary("험로주행모드", categoryId, "https://www.hyundai.com/contents/spec/LX24/tractionmode_s.jpg")));
+
     @Autowired
     public OptionDefaultComponentsTest(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
         this.optionRepository = new OptionRepository(jdbcTemplate);
     }
-
-    Long trimId = 2L;
-    Long categoryId = 1L;
-    int pageSize = 5;
 
     @Test
     @DisplayName("첫번째 페이지의 조회를 검증한다")
@@ -36,44 +46,32 @@ public class OptionDefaultComponentsTest {
         //given
         int targetPage = 1;
 
-        ArrayList<DefaultOptionSummary> expectedDefaultOptionSummaries = new ArrayList<>(Arrays.asList(
-                new DefaultOptionSummary("8단 자동변속기", 1L, "https://www.hyundai.com/contents/spec/LX24/8at_s.jpg"),
-                new DefaultOptionSummary("ISG 시스템", 1L, "https://www.hyundai.com/contents/spec/LX24/isg_s.jpg"),
-                new DefaultOptionSummary("통합주행모드", 1L, "https://www.hyundai.com/contents/spec/LX24/drivemode_s.jpg"),
-                new DefaultOptionSummary("랙구동형 전동식 파워스티어링(R-MDPS)", 1L, "https://www.hyundai.com/contents/spec/LX24/rmdps_s.jpg"),
-                new DefaultOptionSummary("전자식 변속버튼", 1L, "https://www.hyundai.com/contents/spec/LX24/sbw_s.jpg")));
-
-
         //when
         List<DefaultOptionSummary> actualDefaultOptionSummaries =
                 optionRepository.findPaginatedDefaultOptionsByTrimIdAndCategoryId(trimId, categoryId, targetPage, pageSize);
 
         //then
-        Assertions.assertThat(expectedDefaultOptionSummaries)
+        Assertions.assertThat(actualDefaultOptionSummaries)
                 .usingRecursiveComparison()
                 .ignoringCollectionOrder()
-                .isEqualTo(actualDefaultOptionSummaries);
+                .isEqualTo(expectedDefaultOptionSummaries.subList(0, 5));
     }
 
     @Test
     @DisplayName("마지막 페이지의 조회를 검증한다")
     void findLastPage() {
         //given
-        int targetPage = 2;
-
-        ArrayList<DefaultOptionSummary> expectedDefaultOptionSummaries = new ArrayList<>(Arrays.asList(
-                new DefaultOptionSummary("HTARC", 1L, "https://www.hyundai.com/contents/spec/LX24/htrac_s.jpg"),
-                new DefaultOptionSummary("험로주행모드", 1L, "https://www.hyundai.com/contents/spec/LX24/tractionmode_s.jpg")));
+        int targetPage = totalPages;
 
         //when
         List<DefaultOptionSummary> actualDefaultOptionSummaries =
                 optionRepository.findPaginatedDefaultOptionsByTrimIdAndCategoryId(trimId, categoryId, targetPage, pageSize);
 
         //then
-        Assertions.assertThat(expectedDefaultOptionSummaries)
+        Assertions.assertThat(actualDefaultOptionSummaries)
                 .usingRecursiveComparison()
                 .ignoringCollectionOrder()
-                .isEqualTo(actualDefaultOptionSummaries);
+                .isEqualTo(expectedDefaultOptionSummaries.subList(5, 7));
     }
 
 }
