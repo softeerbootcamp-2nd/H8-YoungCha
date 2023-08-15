2e486a5aeabd8f489c012c7d30e977d2ec1167a9
csct3434 <csct3434@gmail.com>
2023-08-15 16:42:34 +0900

    feat: 트림 기본 품목 조회 repository 로직 작성

--- server/src/main/java/team/youngcha/domain/option/repository/OptionRepository.java
+++ server/src/main/java/team/youngcha/domain/option/repository/OptionRepository.java
@@ -4,8 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+import team.youngcha.domain.option.dto.DefaultOptionSummary;
 import team.youngcha.domain.option.entity.Option;
 import team.youngcha.domain.option.entity.OptionType;
+import team.youngcha.domain.trim.enums.TrimOptionType;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -17,6 +19,7 @@ public class OptionRepository {
 
     private final JdbcTemplate jdbcTemplate;
     private final RowMapper<Option> optionRowMapper = new OptionRowMapper();
+    private final RowMapper<DefaultOptionSummary> defaultOptionSummaryRowMapper = new DefaultOptionSummaryRowMapper();
 
     public List<Option> findPowerTrainsByTrimIdAndType(Long trimId, OptionType type) {
 
@@ -37,6 +40,54 @@ public class OptionRepository {
                     .feedback(resultSet.getString("feedback"))
                     .categoryId(resultSet.getLong("category_id"))
                     .build();
+        }
+    }
+
+    public int countDefaultOptionsByTrimIdAndCategoryId(Long trimId, Long categoryId) {
+        String sql = "SELECT COUNT(*) FROM trim " +
+                "JOIN (SELECT * FROM trim_options WHERE type = ?) trim_options ON trim.id = trim_options.trim_id " +
+                "JOIN (SELECT * FROM options WHERE category_id = ?) options ON trim_options.options_id = options.id " +
+                "WHERE trim.id = ?;";
+
+        return jdbcTemplate.queryForObject(sql, Integer.class,
+                OptionType.BASIC.getType(),
+                categoryId,
+                trimId);
+    }
+
+    public List<DefaultOptionSummary> findPaginatedDefaultOptionsByTrimIdAndCategoryId(Long trimId, Long categoryId, int page, int size) {
+        int offset = (page - 1) * size;
+
+        String sql =
+                "SELECT * FROM " +
+                        "(SELECT " +
+                            "options.id AS optionsId, " +
+                            "options.name AS optionsName, " +
+                            "options.category_id AS optionsCategoryId, " +
+                            "options_image.img_url AS optionsImgUrl " +
+                        "FROM trim " +
+                            "JOIN (SELECT * FROM trim_options WHERE type = ?) trim_options ON trim.id = trim_options.trim_id " +
+                            "JOIN (SELECT * FROM options WHERE category_id = ?) options ON trim_options.options_id = options.id " +
+                            "JOIN options_image ON options.id = options_image.options_id " +
+                        "WHERE trim.id = ?" +
+                        ") inner_table " +
+                "ORDER BY optionsId ASC LIMIT ? OFFSET ?";
+
+        return jdbcTemplate.query(sql, defaultOptionSummaryRowMapper,
+                TrimOptionType.DEFAULT.getValue(),
+                categoryId,
+                trimId,
+                size,
+                offset);
+    }
+
+    private class DefaultOptionSummaryRowMapper implements RowMapper<DefaultOptionSummary> {
+        @Override
+        public DefaultOptionSummary mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+            return new DefaultOptionSummary(
+                    resultSet.getString("optionsName"),
+                    resultSet.getLong("optionsCategoryId"),
+                    resultSet.getString("optionsImgUrl"));
         }
     }
 }
