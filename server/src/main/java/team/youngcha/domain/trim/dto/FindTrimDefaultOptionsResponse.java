1f1622fc2d7ed484c5a5ea8adfeada95c665f409
csct3434 <csct3434@gmail.com>
2023-08-16 22:48:25 +0900

    refactor: 역직렬화에 필요한 기본 생성자를 DTO에 추가

--- server/src/main/java/team/youngcha/domain/trim/dto/FindTrimDefaultOptionsResponse.java
+++ server/src/main/java/team/youngcha/domain/trim/dto/FindTrimDefaultOptionsResponse.java
@@ -1,33 +1,36 @@
 package team.youngcha.domain.trim.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import team.youngcha.domain.option.dto.DefaultOptionSummary;
 
 import java.util.List;
 
 @Schema(description = "트림 기본 품목 응답 페이지")
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FindTrimDefaultOptionsResponse {
 
     @Schema(description = "트림 아이디")
-    private final Long trimId;
+    private Long trimId;
 
     @Schema(description = "첫번째 페이지이면 true")
-    private final boolean first;
+    private boolean first;
 
     @Schema(description = "마지막 페이지이면 true")
-    private final boolean last;
+    private boolean last;
 
     @Schema(description = "전체 데이터 개수")
-    private final int totalElements;
+    private int totalElements;
 
     @Schema(description = "전체 페이지 수")
-    private final int totalPages;
+    private int totalPages;
 
     @Schema(description = "기본 품목 정보")
-    private final List<DefaultOptionSummary> contents;
+    private List<DefaultOptionSummary> contents;
 
     @Builder
     public FindTrimDefaultOptionsResponse(Long trimId, boolean first, boolean last, int totalElements, int totalPages, List<DefaultOptionSummary> contents) {
