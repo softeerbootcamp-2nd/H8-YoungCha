e7d4b88eea6610a0bce64e7a897993a6d0cf586d
dohyeon-han <limited.hyeon@gmail.com>
2023-08-16 18:37:09 +0900

    feat: 내장 색상 조회 쿼리 구현
    연관 관계를 고려해서 조회
    외장 색상과 내장 색상과의 연관 관계 테이블이 설정되어 있지 않으면 전체 조회

--- server/src/main/java/team/youngcha/domain/category/enums/SelectiveCategory.java
+++ server/src/main/java/team/youngcha/domain/category/enums/SelectiveCategory.java
@@ -11,10 +11,10 @@ public enum SelectiveCategory {
     INTERIOR_COLOR("내장 색상", "interior_color"),
     WHEEL("휠", "wheel");
 
-    private String name;
-    private String column;
+    private final String name;
+    private final String column;
 
-    SelectiveCategory(String name, String column){
+    SelectiveCategory(String name, String column) {
         this.name = name;
         this.column = column;
     }
--- server/src/main/java/team/youngcha/domain/option/repository/OptionRepository.java
+++ server/src/main/java/team/youngcha/domain/option/repository/OptionRepository.java
@@ -1,8 +1,9 @@
 package team.youngcha.domain.option.repository;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 import team.youngcha.domain.category.enums.SelectiveCategory;
 import team.youngcha.domain.option.entity.Option;
@@ -16,16 +17,47 @@ import java.util.List;
 @RequiredArgsConstructor
 public class OptionRepository {
 
-    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
     private final RowMapper<Option> optionRowMapper = new OptionRowMapper();
 
     public List<Option> findOptionsByTrimIdAndType(Long trimId, OptionType type, SelectiveCategory name) {
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("categoryName", name.getName());
+        params.addValue("trimId", trimId);
+        params.addValue("optionType", type.getType());
 
-        return jdbcTemplate.query("select * from options " +
-                        "join category on options.category_id = category.id and category.name = ? " +
+        return namedParameterJdbcTemplate.query("select * from options " +
+                        "join category on options.category_id = category.id and category.name = (:categoryName) " +
                         "join trim_options on options.id = trim_options.options_id " +
-                        "and trim_options.trim_id = ? and trim_options.type = ? ",
-                optionRowMapper, name.getName(), trimId, type.getType());
+                        "and trim_options.trim_id = (:trimId) and trim_options.type = (:optionType) ",
+                params, optionRowMapper);
+    }
+
+    public List<Option> findInteriorColorsByTrimIdAndExteriorColorId(Long trimId, Long exteriorColorId) {
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("categoryName", SelectiveCategory.INTERIOR_COLOR.getName());
+        params.addValue("trimId", trimId);
+        params.addValue("optionType", OptionType.OPTIONAL.getType());
+        params.addValue("exteriorColorId", exteriorColorId);
+
+        String baseQuery = "select options.id as id, options.name as name, options.price as price, " +
+                "options.feedback as feedback, options.category_id as category_id from options " +
+                "join category on options.category_id = category.id and category.name = (:categoryName) " +
+                "join trim_options on options.id = trim_options.options_id " +
+                "and trim_options.trim_id = (:trimId) and trim_options.type = (:optionType)";
+
+        String fullQuery = baseQuery +
+                " join options_relation on options.id = options_relation.child_id " +
+                "and options_relation.parent_id = (:exteriorColorId)";
+
+
+        List<Option> query = namedParameterJdbcTemplate.query(fullQuery,
+                params, optionRowMapper);
+        if (query.isEmpty()) {
+            return namedParameterJdbcTemplate.query(baseQuery,
+                    params, optionRowMapper);
+        }
+        return query;
     }
 
     private static class OptionRowMapper implements RowMapper<Option> {
