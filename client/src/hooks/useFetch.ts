ddac499390818a2a617fd3931fdef63437939095
bae-sh <b5460881@naver.com>
2023-08-17 15:00:14 +0900

    feat: useFetch hooks 생성

---
+++ client/src/hooks/useFetch.ts
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+
+function useFetch(url: string) {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, [url]);
+
+  return { loading, data, error };
+}
+
+export default useFetch;
