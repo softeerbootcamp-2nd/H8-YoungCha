d8ea96d7e00c3afb27a3b37e2b55f102b245d505
bae-sh <b5460881@naver.com>
2023-08-17 21:36:08 +0900

    chore: 불필요한 icon 삭제

--- client/src/assets/icons/index.ts
+++ client/src/assets/icons/index.ts
@@ -4,7 +4,6 @@ import ModelChangeArrow from './model-change-arrow';
 import DictionaryOffIcon from './dictionary-off-icon';
 import DictionaryOnIcon from './dictionary-on-icon';
 import MainBelowArrow from './main-below-arrow';
-import MainOutsideColor from './main-outside-color';
 import CheckInactive from './select-check-inactive';
 import CheckActive from './select-check';
 import Spanner from './Spanner';
@@ -22,7 +21,6 @@ export {
   DictionaryOffIcon,
   DictionaryOnIcon,
   MainBelowArrow,
-  MainOutsideColor,
   CheckInactive,
   CheckActive,
   Spanner,
