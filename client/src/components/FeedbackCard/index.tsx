12ea2cf1faeef7da4807d324a90bb8ce52aa4f6b
jhyep <pjh1115_@naver.com>
2023-08-17 22:43:19 +0900

    feat: 옵션 피드백 카드 구현

---
+++ client/src/components/FeedbackCard/index.tsx
@@ -0,0 +1,60 @@
+import * as Icon from '@/assets/icons';
+import { powerTrainMock } from '@/assets/mock/optionMock';
+import { useState, useEffect } from 'react';
+
+interface FeedbackCardProps {
+  isSelected: boolean;
+}
+
+function FeedbackCard({ isSelected }: FeedbackCardProps) {
+  const [showSmileIcon, setShowSmileIcon] = useState(true);
+  const [showLaughingIcon, setShowLaughingIcon] = useState(false);
+
+  useEffect(() => {
+    if (isSelected) {
+      const timer1 = setTimeout(() => {
+        setShowSmileIcon(false);
+      }, 500);
+
+      const timer2 = setTimeout(() => {
+        setShowLaughingIcon(true);
+      }, 1000);
+
+      return () => {
+        clearTimeout(timer1);
+        clearTimeout(timer2);
+      };
+    }
+  }, [isSelected]);
+
+  return (
+    <div
+      className={`bg-main-blue border-2 border-main-blue rounded-6px w-375px max-h-150px p-20px`}
+    >
+      <div
+        className={`${
+          showSmileIcon ? 'opacity-100' : 'opacity-0'
+        } transition-opacity duration-500 linear`}
+      >
+        {showSmileIcon && <Icon.SmileIcon />}
+      </div>
+      <div
+        className={`flex ${
+          showLaughingIcon ? 'opacity-100' : 'opacity-0'
+        } transition-opacity duration-500 linear`}
+      >
+        {showLaughingIcon && <Icon.LaughingFaceIcon />}
+        {showLaughingIcon && <Icon.ThumbsUpIcon />}
+      </div>
+
+      <div className="font-medium text-white text-20px mt-10px">
+        {powerTrainMock.feedback[0].title}
+      </div>
+      <div className="text-white body3 mt-6px">
+        {powerTrainMock.feedback[0].description}
+      </div>
+    </div>
+  );
+}
+
+export default FeedbackCard;
