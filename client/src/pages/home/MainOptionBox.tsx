47bd0e7e663b0f1b4240b3bc5972fcb7a4925d3c
bae-sh <b5460881@naver.com>
2023-08-17 21:39:29 +0900

    chore: 핵심 옵션 ( 뒤 이름 삭제 기능 추가

--- client/src/pages/home/MainOptionBox.tsx
+++ client/src/pages/home/MainOptionBox.tsx
@@ -1,6 +1,7 @@
 import { TrimType } from '@/assets/mock/mock';
 import OptionLayout from './OptionLayout';
 import { TITLE } from './constant';
+import removeBracket from '../../utils/removeBracket';
 
 interface MainOptionBoxProps {
   trims: TrimType[];
@@ -20,7 +21,7 @@ function MainOptionBox({ trims }: MainOptionBoxProps) {
               key={`main-option-imgae-${index}`}
             >
               <img src={imgUrl} alt="name" className="w-80px h-60px" />
-              <p className="body3 text-grey-004">{name}</p>
+              <p className="body3 text-grey-004">{removeBracket(name)}</p>
             </li>
           ))}
         </ul>
