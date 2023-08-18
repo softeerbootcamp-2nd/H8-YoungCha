106a41496f4f830b3e64a1c4024ffc9ae9df14e6
bae-sh <b5460881@naver.com>
2023-08-18 17:27:30 +0900

    design: 반응형 width 수정

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -28,7 +28,7 @@ function Home() {
       {!loading && (
         <>
           <div className="flex justify-center w-full h-screen bg-center bg-cover pt-85px bg-main-background-image">
-            <div className="flex flex-col justify-between px-128px max-w-7xl py-16px">
+            <div className="flex flex-col justify-between max-w-5xl py-16px">
               <div>
                 <p className="text-white text-24px font-hsans-head tracking-[-0.96px] leading-[31.2px]">
                   {TEXT.MAKING_MY_CAR}
@@ -58,13 +58,13 @@ function Home() {
           </div>
           <div className="w-full pb-400px">
             <CarsNameListBox trims={trims} />
-            <div className="flex flex-col m-auto max-w-7xl pt-32px gap-60px px-128px">
+            <div className="flex flex-col items-center pt-32px gap-60px ">
               <CarsImageBox trims={trims} />
-              <div className="flex flex-col gap-48px">
+              <div className="flex flex-col max-w-5xl gap-48px">
                 <MainOptionBox trims={trims} />
                 <ExteriorColorBox trims={trims} />
                 <InternalColorBox trims={trims} />
-                <BasicOptionBox />
+                <BasicOptionBox trims={trims} />
                 <MakingCarButtonsBox />
                 <GuideModeButton />
               </div>
