1cba5e88e0a987dc66d67cfacac3c505aab51273
dohyeon-han <limited.hyeon@gmail.com>
2023-08-18 12:27:16 +0900

    feat: 구동 방식, 바디 타입 가이드 모드 controller 구현

--- server/src/main/java/team/youngcha/domain/option/controller/GuideOptionController.java
+++ server/src/main/java/team/youngcha/domain/option/controller/GuideOptionController.java
@@ -41,9 +41,59 @@ public class GuideOptionController {
             @Schema(description = "3순위 키워드 아이디")
             @RequestParam Long keyword3Id
     ) {
+        GuideInfo guideInfo =
+                new GuideInfo(gender, ageRange, List.of(keyword1Id, keyword2Id, keyword3Id));
+        return findGuideOptions(trimId, guideInfo, SelectiveCategory.POWER_TRAIN);
+    }
+
+    @Operation(summary = "구동 방식 가이드 모드 옵션 조회",
+            description = "가이드 모드에서 구동 방식의 옵션을 유사 사용자 선택량과 함께 조회합니다.")
+    @GetMapping("/driving-system")
+    public ResponseEntity<SuccessResponse<List<FindGuideOptionResponse>>> findGuideDrivingSystem(
+            @Schema(description = "트림 아이디")
+            @PathVariable Long trimId,
+            @Schema(description = "성별 (남자: 0, 여자: 1, 선택 안함: 2)")
+            @RequestParam Gender gender,
+            @Schema(description = "나이 (20대 ~ 70대 이상, 2, 3,..., 7)")
+            @RequestParam(name = "age") AgeRange ageRange,
+            @Schema(description = "1순위 키워드 아이디")
+            @RequestParam Long keyword1Id,
+            @Schema(description = "2순위 키워드 아이디")
+            @RequestParam Long keyword2Id,
+            @Schema(description = "3순위 키워드 아이디")
+            @RequestParam Long keyword3Id
+    ) {
+        GuideInfo guideInfo =
+                new GuideInfo(gender, ageRange, List.of(keyword1Id, keyword2Id, keyword3Id));
+        return findGuideOptions(trimId, guideInfo, SelectiveCategory.DRIVING_SYSTEM);
+    }
+
+    @Operation(summary = "바디 타입 가이드 모드 옵션 조회",
+            description = "가이드 모드에서 바디 타입의 옵션을 유사 사용자 선택량과 함께 조회합니다.")
+    @GetMapping("/body-type")
+    public ResponseEntity<SuccessResponse<List<FindGuideOptionResponse>>> findGuideBodyType(
+            @Schema(description = "트림 아이디")
+            @PathVariable Long trimId,
+            @Schema(description = "성별 (남자: 0, 여자: 1, 선택 안함: 2)")
+            @RequestParam Gender gender,
+            @Schema(description = "나이 (20대 ~ 70대 이상, 2, 3,..., 7)")
+            @RequestParam(name = "age") AgeRange ageRange,
+            @Schema(description = "1순위 키워드 아이디")
+            @RequestParam Long keyword1Id,
+            @Schema(description = "2순위 키워드 아이디")
+            @RequestParam Long keyword2Id,
+            @Schema(description = "3순위 키워드 아이디")
+            @RequestParam Long keyword3Id
+    ) {
         GuideInfo guideInfo = new GuideInfo(gender, ageRange, List.of(keyword1Id, keyword2Id, keyword3Id));
+        return findGuideOptions(trimId, guideInfo, SelectiveCategory.BODY_TYPE);
+    }
+
+    private ResponseEntity<SuccessResponse<List<FindGuideOptionResponse>>> findGuideOptions(Long trimId,
+                                                                                            GuideInfo guideInfo,
+                                                                                            SelectiveCategory category) {
         List<FindGuideOptionResponse> findGuideOptionResponses = optionService
-                .findGuideOptions(trimId, guideInfo, SelectiveCategory.POWER_TRAIN);
+                .findGuideOptions(trimId, guideInfo, category);
         SuccessResponse<List<FindGuideOptionResponse>> successResponse =
                 new SuccessResponse<>(findGuideOptionResponses);
         return ResponseEntity.ok(successResponse);
