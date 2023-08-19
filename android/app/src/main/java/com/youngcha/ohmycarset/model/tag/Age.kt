a9f142153247433b508f4cb7dedc50b2c65aa80f
HR <dev.muun07@gmail.com>
2023-08-20 05:48:16 +0900

    chore: 불필요한 파일 삭제

--- android/app/src/main/java/com/youngcha/ohmycarset/model/tag/Age.kt
+++
@@ -1,9 +0,0 @@
-package com.youngcha.ohmycarset.model.tag
-
-data class Age(val age:String) {
-    var isSelected:Boolean =false
-
-    override fun toString(): String {
-        return age.toString()
-    }
-}
--- android/app/src/main/java/com/youngcha/ohmycarset/model/tag/Important.kt
+++
@@ -1,9 +0,0 @@
-package com.youngcha.ohmycarset.model.tag
-
-data class Important(val important:String) {
-    var isSelected:Boolean =false
-
-    override fun toString(): String {
-        return important.toString()
-    }
-}
