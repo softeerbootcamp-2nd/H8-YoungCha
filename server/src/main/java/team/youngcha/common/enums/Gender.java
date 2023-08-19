671a9df353b828b59711f9391546aa2d3859c0df
csct3434 <csct3434@gmail.com>
2023-08-20 06:28:14 +0900

    feat: Gender 열거형 속성 및 메서드 추가
    - 성별의 이름을 나타내는 name 속성 추가
    - 성별 별 키워드 이름을 반환하는 메서드 추가

--- server/src/main/java/team/youngcha/common/enums/Gender.java
+++ server/src/main/java/team/youngcha/common/enums/Gender.java
@@ -1,22 +1,31 @@
 package team.youngcha.common.enums;
 
-import lombok.Getter;
-
 import java.util.Arrays;
 
-@Getter
 public enum Gender {
-    MALE(0), FEMALE(1), NONE(2);
+    MALE(0, "남성"),
+    FEMALE(1, "여성"),
+    NONE(2, "선택 안함");
 
     private final int type;
+    private final String name;
 
-    Gender(int type) {
+    Gender(int type, String name) {
         this.type = type;
+        this.name = name;
     }
 
     public static Gender of(int type) {
         return Arrays.stream(Gender.values())
                 .filter(gender -> gender.getType() == type)
                 .findAny().orElseThrow(() -> new IllegalArgumentException("일치하는 gender가 없습니다."));
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public String toKeyword() {
+        return this.name;
     }
 }
