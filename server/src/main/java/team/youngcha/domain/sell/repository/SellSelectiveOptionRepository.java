4b34801515b3efd765dca4c0f9c16120c7f8524b
csct3434 <csct3434@gmail.com>
2023-08-20 01:43:46 +0900

    feat: 특정 트림의 선택 옵션별 판매량을 조회하는 repository 로직 작성

--- server/src/main/java/team/youngcha/domain/sell/repository/SellSelectiveOptionRepository.java
+++ server/src/main/java/team/youngcha/domain/sell/repository/SellSelectiveOptionRepository.java
@@ -1,16 +1,33 @@
 package team.youngcha.domain.sell.repository;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
-import team.youngcha.domain.sell.entity.SellSelectiveOption;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
 public class SellSelectiveOptionRepository {
 
-    private final JdbcTemplate jdbcTemplate;
-    private final RowMapper<SellSelectiveOption> sellSelectiveOptionRowMapper = BeanPropertyRowMapper.newInstance(SellSelectiveOption.class);
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    public Map<Long, Long> countByOptionIdForTrim(Long trimId) {
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("trimId", trimId);
+
+        String sql = "SELECT options_id, COUNT(*) AS options_count " +
+                "FROM sell_selective_options " +
+                "WHERE sell_id IN (SELECT id FROM sell WHERE trim_id = (:trimId)) " +
+                "GROUP BY options_id";
+
+        return namedParameterJdbcTemplate.queryForList(sql, params)
+                .stream()
+                .collect(Collectors.toMap(
+                        row -> (Long) row.get("options_id"),
+                        row -> (Long) row.get("options_count")
+                ));
+    }
 }
