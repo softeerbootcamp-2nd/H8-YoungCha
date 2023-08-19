bdbbac07e8d1e9ce29f0568e58fbc9456f434167
1lsang <ilsangpark.98@gmail.com>
2023-08-19 14:41:19 +0900

    design: Header 스타일 변경

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -23,9 +23,9 @@ function Header() {
 
   return (
     <header
-      className={`${backgroundColor} z-20 transition-colors duration-300 ease-out fixed top-0 w-full h-80px select-none min-w-768px`}
+      className={`${backgroundColor} z-20 transition-colors duration-300 ease-out fixed top-0 w-full h-80px select-none min-w-768px px-32px`}
     >
-      <div className="flex items-center justify-between h-full max-w-5xl mx-auto xl:max-w-none px-32px xl:px-160px">
+      <div className="flex items-center justify-between h-full max-w-5xl mx-auto xl:max-w-none xl:px-96px">
         <div className="flex h-24px gap-20px">
           <Link to="/">
             <Icon.HDLogo className="w-45px lg:w-166px h-23px" />
