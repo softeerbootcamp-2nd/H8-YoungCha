39ec779a00dfac6a7c8d7834809453189c3d166d
csct3434 <csct3434@gmail.com>
2023-08-21 22:07:29 +0900

    feat: EstimateSelectiveOptionRepository.countIfEstimateIncludeOption()
    메서드에 전달된 견적 중 특정 옵션을 포함한 견적의 수를 조회하는 메서드 작성

--- server/src/main/java/team/youngcha/domain/estimate/repository/EstimateSelectiveOptionRepository.java
+++ server/src/main/java/team/youngcha/domain/estimate/repository/EstimateSelectiveOptionRepository.java
@@ -2,15 +2,33 @@ package team.youngcha.domain.estimate.repository;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 import team.youngcha.domain.estimate.entity.EstimateSelectiveOption;
+import team.youngcha.domain.option.entity.Option;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class EstimateSelectiveOptionRepository {
 
-    private final JdbcTemplate jdbcTemplate;
-    private final RowMapper<EstimateSelectiveOption> estimateSelectiveOptionRowMapper = BeanPropertyRowMapper.newInstance(EstimateSelectiveOption.class);
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private final RowMapper<EstimateSelectiveOption> estimateSelectiveOptionRowMapper
+            = BeanPropertyRowMapper.newInstance(EstimateSelectiveOption.class);
+
+
+    public Long countIfEstimateIncludeOption(List<Long> estimateIds, Option option) {
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("estimateIds", estimateIds);
+        params.addValue("optionId", option.getId());
+
+        String sql = "SELECT COUNT(*) FROM estimate_selective_options e " +
+                "WHERE e.estimate_id IN (:estimateIds) " +
+                "AND e.options_id = :optionId";
+
+        return namedParameterJdbcTemplate.queryForObject(sql, params, Long.class);
+    }
 }
