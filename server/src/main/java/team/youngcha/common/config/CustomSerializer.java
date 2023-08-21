a71aef5c0e263d5797816a98c7af162aa9530af1
dohyeon-han <limited.hyeon@gmail.com>
2023-08-21 22:34:56 +0900

    feat: cache manager bean 추가
    Map<Long, Long>으로 역직렬화하기 위한 manger 설정

---
+++ server/src/main/java/team/youngcha/common/config/CustomSerializer.java
@@ -0,0 +1,19 @@
+package team.youngcha.common.config;
+
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+
+import java.util.Map;
+
+public class CustomSerializer extends Jackson2JsonRedisSerializer {
+    public CustomSerializer(Class type) {
+        super(type);
+    }
+
+    @Override
+    protected JavaType getJavaType(Class clazz) {
+        return TypeFactory.defaultInstance()
+                .constructMapType(Map.class, Long.class, Long.class);
+    }
+}
--- server/src/main/java/team/youngcha/common/config/RedisConfig.java
+++ server/src/main/java/team/youngcha/common/config/RedisConfig.java
@@ -5,6 +5,7 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -14,6 +15,7 @@ import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
+import java.util.Map;
 
 @Configuration
 @EnableCaching
@@ -31,15 +33,38 @@ public class RedisConfig {
     }
 
     @Bean
-    public CacheManager cacheManager(RedisConnectionFactory cf) {
+    @Primary
+    public CacheManager defaultCacheManager(RedisConnectionFactory cf) {
         RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration
                 .defaultCacheConfig()
                 .serializeKeysWith(RedisSerializationContext.SerializationPair
                         .fromSerializer(new StringRedisSerializer()))
                 .serializeValuesWith(RedisSerializationContext.SerializationPair
                         .fromSerializer(new GenericJackson2JsonRedisSerializer()))
-                .entryTtl(Duration.ofMinutes(6L)); // 1시간 캐시
+                .entryTtl(Duration.ofMinutes(1L)); // 1시간 캐시
 
-        return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(cf).cacheDefaults(redisCacheConfiguration).build();
+        return buildCacheManager(cf, redisCacheConfiguration);
+    }
+
+    // CustomSerializer를 사용해 Map<Long,Long>로 역직렬화
+    @Bean
+    public CacheManager mapCacheManager(RedisConnectionFactory cf) {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration
+                .defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(new CustomSerializer(Map.class)))
+                .entryTtl(Duration.ofMinutes(1L)); // 1시간 캐시
+
+        return buildCacheManager(cf, redisCacheConfiguration);
+    }
+
+    private CacheManager buildCacheManager(RedisConnectionFactory cf,
+                                           RedisCacheConfiguration redisCacheConfiguration) {
+        return RedisCacheManager.RedisCacheManagerBuilder.
+                fromConnectionFactory(cf)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
     }
 }
