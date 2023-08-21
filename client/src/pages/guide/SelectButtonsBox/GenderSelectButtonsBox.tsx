bb5d73cf95cd517774c8d22dac8650b56f643d5f
jhyep <pjh1115_@naver.com>
2023-08-21 10:52:27 +0900

    feat: 선택된 gender의 경우 active하게 렌더

--- client/src/pages/guide/SelectButtonsBox/GenderSelectButtonsBox.tsx
+++ client/src/pages/guide/SelectButtonsBox/GenderSelectButtonsBox.tsx
@@ -1,32 +1,47 @@
 import ActiveCheck from '@/assets/icons/select-check';
 import InactiveCheck from '@/assets/icons/select-check-inactive';
 import SelectButton from '@/components/SelectButton';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { GenderType } from '../type';
+import { Fragment, useState } from 'react';
+import { useTagSelectContext } from '@/store/useTagSelectContext';
 
-type GenderType = '남성' | '여성' | '선택 안함';
 const genderSelectArray: Array<GenderType> = ['남성', '여성', '선택 안함'];
 
 function GenderSelectButtonsBox() {
   const [hoveredGender, setHoveredGender] = useState<GenderType | ''>('');
+  const { selectedGender, setSelectedGender } = useTagSelectContext();
+
+  function handleOnclick(Gender: GenderType) {
+    setSelectedGender(Gender);
+  }
 
   return (
     <div className={`flex flex-col w-343px gap-12px mt-173px`}>
       {genderSelectArray.map((Gender: GenderType) => {
         const isHovered = hoveredGender === Gender;
+        const isSelected = selectedGender === Gender;
 
         return (
-          <Link to="/model/LX06/guide/keyword" key={Gender}>
+          <Fragment key={Gender}>
             <SelectButton
-              type={isHovered ? 'iconActive' : 'default'}
+              type={
+                isSelected ? 'iconActive' : isHovered ? 'iconActive' : 'default'
+              }
               size={'lg'}
               onMouseEnter={() => setHoveredGender(Gender)}
               onMouseLeave={() => setHoveredGender('')}
+              onClick={() => handleOnclick(Gender)}
             >
               {Gender}
-              {isHovered ? <ActiveCheck /> : <InactiveCheck />}
+              {isSelected ? (
+                <ActiveCheck />
+              ) : isHovered ? (
+                <ActiveCheck />
+              ) : (
+                <InactiveCheck />
+              )}
             </SelectButton>
-          </Link>
+          </Fragment>
         );
       })}
     </div>
