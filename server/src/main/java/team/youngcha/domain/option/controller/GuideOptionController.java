ebfb58afe82f2e97ee1b3c09b7729573ad1a186e
csct3434 <csct3434@gmail.com>
2023-08-21 16:00:23 +0900

    feat: GuideOptionController.findSelectiveOptions()
    가이드 모드 선택 옵션 조회 API controller 메서드 작성

--- server/src/main/java/team/youngcha/domain/option/controller/GuideOptionController.java
+++ server/src/main/java/team/youngcha/domain/option/controller/GuideOptionController.java
@@ -162,6 +162,30 @@ public class GuideOptionController {
         return ResponseEntity.ok(successResponse);
     }
 
+    @Operation(summary = "파워 트레인 가이드 모드 옵션 조회",
+            description = "가이드 모드에서 파워 트레인의 옵션을 유사 사용자 선택량과 함께 조회합니다.")
+    @GetMapping("/options")
+    public ResponseEntity<SuccessResponse<List<FindGuideOptionResponse>>> findSelectiveOptions(
+            @Schema(description = "트림 아이디")
+            @PathVariable Long trimId,
+            @Schema(description = "성별 (남자: 0, 여자: 1, 선택 안함: 2)")
+            @RequestParam Gender gender,
+            @Schema(description = "나이 (20대 ~ 70대 이상, 2, 3,..., 7)")
+            @RequestParam(name = "age") AgeRange ageRange,
+            @Schema(description = "1순위 키워드 아이디")
+            @RequestParam Long keyword1Id,
+            @Schema(description = "2순위 키워드 아이디")
+            @RequestParam Long keyword2Id,
+            @Schema(description = "3순위 키워드 아이디")
+            @RequestParam Long keyword3Id
+    ) {
+        GuideInfo guideInfo =
+                new GuideInfo(gender, ageRange, List.of(keyword1Id, keyword2Id, keyword3Id));
+        List<FindGuideOptionResponse> findGuideOptionResponses = optionService.findGuideSelectiveOptions(trimId, guideInfo);
+        SuccessResponse<List<FindGuideOptionResponse>> successResponse = new SuccessResponse<>(findGuideOptionResponses);
+        return ResponseEntity.ok(successResponse);
+    }
+
     private ResponseEntity<SuccessResponse<List<FindGuideOptionResponse>>> findGuideRequiredOptions(Long trimId,
                                                                                                     GuideInfo guideInfo,
                                                                                                     RequiredCategory category) {
