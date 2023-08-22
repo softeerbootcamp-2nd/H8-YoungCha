95654bd19b2d5209b1e55d0d681289392a73335a
jhyep <pjh1115_@naver.com>
2023-08-22 16:46:11 +0900

    feat: 가이드 모드 로딩 페이지 예외 처리

--- client/src/pages/making/index.tsx
+++ client/src/pages/making/index.tsx
@@ -1,8 +1,9 @@
-import { useParams } from 'react-router-dom';
+import { LAST_STEP } from './constant';
+import { useLocation, useParams } from 'react-router-dom';
 import SelectOptionPage from './select/SelectOptionPage';
 import ProgressBar from '@/components/ProgressBar';
-import { LAST_STEP } from './constant';
 import CompleteOptionPageWithLoading from './complete/CompleteOptionPageWithLoading';
+import CompleteOptionPage from './complete/CompleteOptionPage';
 
 interface MakingPageProps {
   path: 'self' | 'guide';
@@ -11,12 +12,16 @@ interface MakingPageProps {
 function MakingPage({ path }: MakingPageProps) {
   const { step, id } = useParams() as { step: string; id: string };
   const currentStep = Number(step);
+  const { state } = useLocation();
+  console.log(state);
 
   return (
     <div className="flex flex-col w-full h-full">
       <ProgressBar step={currentStep} mode={path} id={id} />
       {currentStep < LAST_STEP ? (
         <SelectOptionPage path={path} />
+      ) : state?.isGuide ? (
+        <CompleteOptionPage />
       ) : (
         <CompleteOptionPageWithLoading />
       )}
