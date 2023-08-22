877a3ba72e95c91e0a59180b4926425198daef9a
HyeonSeongKang <gt9697@naver.com>
2023-08-22 13:57:08 +0900

    chore: 코드 정리

--- android/app/src/main/java/com/youngcha/ohmycarset/ui/customview/HyundaiButtonView.kt
+++ android/app/src/main/java/com/youngcha/ohmycarset/ui/customview/HyundaiButtonView.kt
@@ -2,6 +2,7 @@ package com.youngcha.ohmycarset.ui.customview
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.ViewPropertyAnimator
