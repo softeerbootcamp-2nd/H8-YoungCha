98bd0b887af54467db8f5880a7d1e9a3bc3e41b2
dohyeon-han <limited.hyeon@gmail.com>
2023-08-22 22:43:04 +0900

    fix: 배포에서 nginx 연결 profile 확인 api 수정

--- server/script/profile.sh
+++ server/script/profile.sh
@@ -2,14 +2,14 @@
 
 # nginx와 연결되지 않은 profile 찾기(활성화시킬 profile)
 function find_idle_profile() {
-  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profiles)
+  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" https://api.youngcha.team/profiles)
 
   if [ "$RESPONSE_CODE" -ge 400 ]
   then
     # nginx와 연결된 컨테이너가 없으면 spring2가 연결되어 있다고 설정
     CURRENT_PROFILE=spring2
   else
-    CURRENT_PROFILE=$(curl -s http://localhost/profiles)
+    CURRENT_PROFILE=$(curl -s https://api.youngcha.team/profiles)
   fi
 
   # 구동할 profile 설정
