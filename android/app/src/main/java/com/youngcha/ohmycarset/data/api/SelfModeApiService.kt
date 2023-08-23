864e7dad63d162d80eac1d83adf8baf6112e3a94
HyeonSeongKang <gt9697@naver.com>
2023-08-23 16:44:28 +0900

    feat: SelfModeApiService 구현

--- android/app/src/main/java/com/youngcha/ohmycarset/data/api/SelfModeApiService.kt
+++ android/app/src/main/java/com/youngcha/ohmycarset/data/api/SelfModeApiService.kt
@@ -1,6 +1,6 @@
 package com.youngcha.ohmycarset.data.api
 
-import com.youngcha.ohmycarset.data.dto.Option
+import com.youngcha.ohmycarset.data.dto.Component
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -8,26 +8,26 @@ import retrofit2.http.Query
 interface SelfModeApiService {
 
     @GET("/car-make/{id}/self/power-train")
-    suspend fun getPowerTrain(@Path("id") id: String): Option
+    suspend fun getPowerTrain(@Path("id") id: Int): Component
 
     @GET("/car-make/{id}/self/driving-system")
-    suspend fun getDrivingSystem(@Path("id") id: String): Option
+    suspend fun getDrivingSystem(@Path("id") id: Int): Component
 
     @GET("/car-make/{id}/self/body-type")
-    fun getBodyType(@Path("id") id: Int): Option
+    suspend fun getBodyType(@Path("id") id: Int): Component
 
     @GET("/car-make/{id}/self/exterior-color")
-    fun getExteriorColor(@Path("id") id: Int): Option
+    suspend fun getExteriorColor(@Path("id") id: Int): Component
 
     @GET("/car-make/{id}/self/interior-color")
-    fun getInteriorColor(
+    suspend fun getInteriorColor(
         @Path("id") id: Int,
         @Query("exteriorColorId") exteriorColorId: Int
-    ): Option
+    ): Component
 
     @GET("/car-make/{id}/self/wheel")
-    fun getWheel(@Path("id") id: Int): Option
+    suspend fun getWheel(@Path("id") id: Int): Component
 
     @GET("/car-make/{id}/self/options")
-    fun getOptions(@Path("id") id: Int): Option
+    suspend fun getOptions(@Path("id") id: Int): Component
 }
