c04ba50b666b391641c584036e31535ac0bc0517
HyeonSeongKang <gt9697@naver.com>
2023-08-23 15:37:25 +0900

    feat: retrofit client 구현

--- android/app/src/main/java/com/youngcha/ohmycarset/data/api/RetrofitClient.kt
+++ android/app/src/main/java/com/youngcha/ohmycarset/data/api/RetrofitClient.kt
@@ -7,11 +7,14 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitClient {
     private const val BASE_URL = "https://api.youngcha.team"
 
-    val apiService: ApiService by lazy {
+    private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-            .create(ApiService::class.java)
+    }
+
+    val selfModeApi: SelfModeApiService by lazy {
+        retrofit.create(SelfModeApiService::class.java)
     }
 }
