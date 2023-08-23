7799fa7ee8b6a29a2d0638b52d549636a93e8c4a
bae-sh <b5460881@naver.com>
2023-08-23 11:40:26 +0900

    refactor: 초기 유저 값 상수 파일 이동

--- client/src/pages/making/index.tsx
+++ client/src/pages/making/index.tsx
@@ -2,7 +2,7 @@ import { useLocation, useParams } from 'react-router-dom';
 import SelectOptionPage from './select/SelectOptionPage';
 import CompleteOptionPage from './complete/CompleteOptionPage';
 import CompleteOptionPageWithLoading from './complete/CompleteOptionPageWithLoading';
-import { LAST_STEP } from './constant';
+import { INITIAL_USER_SELECTED_DATA, LAST_STEP } from './constant';
 import { UserSelectedOptionDataType } from './type';
 import { Dispatch, SetStateAction, createContext, useState } from 'react';
 
@@ -12,73 +12,6 @@ export interface UserSelectedOptionDataContextType {
     SetStateAction<UserSelectedOptionDataType>
   >;
 }
-
-const INITIAL_USER_SELECTED_DATA: UserSelectedOptionDataType = {
-  mainOptions: {
-    title: '팰리세이드 Le Blanc (르블랑)',
-    options: {
-      trim: {
-        type: '트림',
-        name: 'Le Blanc',
-        imgUrl: '/src/assets/mock/images/palisade.png',
-        price: 1000000,
-      },
-      powerTrain: {
-        type: '파워트레인',
-        name: '디젤 2.2',
-        imgUrl: '/src/assets/mock/images/palisade.png',
-        price: 1000000,
-        categoryId: 1,
-      },
-      drivingSystem: {
-        type: '구동 방식',
-        name: '2WD',
-        imgUrl: '/src/assets/mock/images/palisade.png',
-        price: 1000000,
-        categoryId: 2,
-      },
-      bodyType: {
-        type: '바디 타입',
-        name: '7인승',
-        imgUrl: '/src/assets/mock/images/palisade.png',
-        price: 1000000,
-        categoryId: 3,
-      },
-      wheel: {
-        type: '휠',
-        name: '20인치',
-        imgUrl: '/src/assets/mock/images/palisade.png',
-        price: 1000000,
-        categoryId: 6,
-      },
-    },
-  },
-
-  colors: {
-    title: '색상',
-    options: {
-      exteriorColor: {
-        type: '외장 색상',
-        name: '크리미 화이트 펄',
-        imgUrl: '/src/assets/mock/images/palisade.png',
-        price: 1000000,
-        categoryId: 4,
-      },
-      interiorColor: {
-        type: '내장 색상',
-        name: '퀼팅천연 (블랙)',
-        imgUrl: '/src/assets/mock/images/palisade.png',
-        price: 1000000,
-        categoryId: 5,
-      },
-    },
-  },
-
-  selectedOptions: {
-    title: '옵션',
-    options: [],
-  },
-};
 
 export const UserSelectedOptionDataContext =
   createContext<UserSelectedOptionDataContextType>({
