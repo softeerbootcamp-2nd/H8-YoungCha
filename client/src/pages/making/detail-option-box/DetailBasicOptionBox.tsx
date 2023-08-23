c60d85aaee45c6e2fdc89d01687453663a9ae949
bae-sh <b5460881@naver.com>
2023-08-23 15:52:36 +0900

    feat: useBasicOption 훅 생성 및 적용

--- client/src/pages/making/detail-option-box/DetailBasicOptionBox.tsx
+++ client/src/pages/making/detail-option-box/DetailBasicOptionBox.tsx
@@ -1,12 +1,26 @@
 import { useState } from 'react';
 import DetailOption from './DetailOption';
-import { mockBasicOptions } from '@/assets/mock/mock';
 import { BasicOptionsId } from '@/constant';
 import { BasicOptionFilterType } from '@/types';
+import Button from '@/components/Button';
+import { MoreViewArrow } from '@/assets/icons';
+import useBasicOption from '@/hooks/useBasicOption';
+
+const OPTION_SIZE = 5;
 
 function DetailBasicOptionBox() {
   const [selectedOptionFilter, setSelectedOptionFilter] =
     useState<BasicOptionFilterType>('전체');
+  const [currentSize, setCurrentSize] = useState(OPTION_SIZE);
+  const { basicOptions } = useBasicOption({
+    id: 2,
+    categoryId: BasicOptionsId[selectedOptionFilter],
+    currentSize,
+  });
+
+  function handleMoreOptionClick() {
+    setCurrentSize((prev) => prev + OPTION_SIZE);
+  }
 
   function isSelectOption(categoryId: number) {
     return (
@@ -14,7 +28,7 @@ function DetailBasicOptionBox() {
       BasicOptionsId[selectedOptionFilter] === categoryId
     );
   }
-
+  console.log(basicOptions);
   return (
     <DetailOption>
       <DetailOption.Header
@@ -25,14 +39,23 @@ function DetailBasicOptionBox() {
         selectedOption={selectedOptionFilter}
         onClick={(option) => setSelectedOptionFilter(option)}
       />
-      {mockBasicOptions.contents
-        .filter((option) => isSelectOption(option.categoryId!))
+      {basicOptions?.contents
+        ?.filter((option) => isSelectOption(option.categoryId!))
         .map((option) => (
           <DetailOption.List
             option={{ ...option, type: '기본 포함 품목' }}
             key={option.name}
           />
         ))}
+
+      {!basicOptions.last && (
+        <div className="flex justify-center">
+          <Button color="grey" onClick={handleMoreOptionClick}>
+            더보기
+            <MoreViewArrow />
+          </Button>
+        </div>
+      )}
     </DetailOption>
   );
 }
