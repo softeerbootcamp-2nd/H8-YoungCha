117ed024f83b4f1e73f37d21cbd843d5769691ef
HyeonSeongKang <gt9697@naver.com>
2023-08-24 22:29:53 +0900

    chore: 코드 정리 및 리소스 추가

--- android/app/src/main/java/com/youngcha/ohmycarset/data/model/car/Car.kt
+++ android/app/src/main/java/com/youngcha/ohmycarset/data/model/car/Car.kt
@@ -1,19 +1,10 @@
 package com.youngcha.ohmycarset.data.model.car
 
-//data class Car(
-//    val name: String, // 자동차 이름 ex: 팰리세이드
-//    val type: String, // 자동차 타입 ex: SUV
-//    val mainOptions: List<Map<String, List<OptionInfo>>>, // 자동차 옵션 ex: key: "파워트레인" value: {디젤, 가솔린}
-//  //  val mainOptionImages: List<Map<String, Int>>, // 자동차 옵션 이미지
-//    val subOptions: List<Map<String, List<OptionInfo>>>// 선택 옵션 부분
-//   // val subOptionImage: Map<String, Int>
-//)
-
 data class Car(
-    val name: String,
-    val type: String,
-    val mainOptions: List<Map<String, List<OptionInfo>>>,
-    val subOptions: List<Map<String, List<OptionInfo>>>
+    val name: String = "팰리세이드",
+    val type: String = "SUV",
+    val mainOptions: List<Map<String, List<OptionInfo>>> = emptyList(),
+    val subOptions: List<Map<String, List<OptionInfo>>> = emptyList()
 ) {
     override fun toString(): String {
         val mainOptionsString = mainOptions.joinToString("\n") { entry ->
--- android/app/src/main/java/com/youngcha/ohmycarset/ui/activity/MainActivity.kt
+++ android/app/src/main/java/com/youngcha/ohmycarset/ui/activity/MainActivity.kt
@@ -2,17 +2,29 @@ package com.youngcha.ohmycarset.ui.activity
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.youngcha.ohmycarset.data.api.CategoriesApiService
 import com.youngcha.ohmycarset.databinding.ActivityMainBinding
 import com.youngcha.ohmycarset.data.api.RetrofitClient
+import com.youngcha.ohmycarset.data.api.SelfModeApiService
+import com.youngcha.ohmycarset.data.dto.Category
+import com.youngcha.ohmycarset.data.dto.CategoryDTO
+import com.youngcha.ohmycarset.data.dto.ComponentDTO
+import com.youngcha.ohmycarset.data.model.car.Car
+import com.youngcha.ohmycarset.data.model.car.OptionInfo
+import com.youngcha.ohmycarset.enums.AdditionalTab
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val apiService = RetrofitClient.apiService
     private val mainScope = CoroutineScope(Dispatchers.Main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -40,6 +52,5 @@ class MainActivity : AppCompatActivity() {
                 // 처리
             }
         }
-
     }
 }
