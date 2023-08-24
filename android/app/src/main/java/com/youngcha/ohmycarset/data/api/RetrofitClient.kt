3fcccaa085f073dd0b8f5d38c472ba7809984ccf
HyeonSeongKang <gt9697@naver.com>
2023-08-24 22:22:51 +0900

    refactor: OKHTTP 추가

--- android/app/src/main/java/com/youngcha/ohmycarset/data/api/RetrofitClient.kt
+++ android/app/src/main/java/com/youngcha/ohmycarset/data/api/RetrofitClient.kt
@@ -1,16 +1,27 @@
 package com.youngcha.ohmycarset.data.api
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 // RetrofitClient.kt
 object RetrofitClient {
     private const val BASE_URL = "https://api.youngcha.team"
 
+    // OkHttpClient 인스턴스 생성 및 timeout 설정
+    private val okHttpClient: OkHttpClient by lazy {
+        OkHttpClient.Builder()
+            .readTimeout(60, TimeUnit.SECONDS)  // 읽기 timeout 60초
+            .connectTimeout(60, TimeUnit.SECONDS)  // 연결 timeout 60초
+            .build()
+    }
+
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)  // Retrofit에 OkHttpClient 설정
             .build()
     }
 
