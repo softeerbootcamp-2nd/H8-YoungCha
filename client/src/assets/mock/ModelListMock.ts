a27045e839c9fd02e882f4aa879f971d20eb9293
jhyep <pjh1115_@naver.com>
2023-08-24 15:42:23 +0900

    feat: 모델 리스트 팝업에 사용되는 mock data 작성

---
+++ client/src/assets/mock/ModelListMock.ts
@@ -0,0 +1,38 @@
+export interface ModelInfo {
+  name: string;
+  price: number;
+  imgSrc: string;
+}
+
+export const hydrogenElectricCars: ModelInfo[] = [
+  {
+    name: '넥쏘',
+    price: 69600000,
+    imgSrc: '/src/assets/mock/icons/nexo.svg',
+  },
+  {
+    name: '디 올 뉴 코나 Electric',
+    price: 44520000,
+    imgSrc: '/src/assets/mock/icons/the-all-new-kona.svg',
+  },
+  {
+    name: '아이오닉 6',
+    price: 52000000,
+    imgSrc: '/src/assets/mock/icons/the-all-new-kona.svg',
+  },
+  {
+    name: '아이오닉 5',
+    price: 50050000,
+    imgSrc: '/src/assets/mock/icons/nexo.svg',
+  },
+  {
+    name: '포터 II Electric',
+    price: 69600000,
+    imgSrc: '/src/assets/mock/icons/nexo.svg',
+  },
+  {
+    name: '포터 II Electric 내장탑차',
+    price: 44520000,
+    imgSrc: '/src/assets/mock/icons/the-all-new-kona.svg',
+  },
+];
