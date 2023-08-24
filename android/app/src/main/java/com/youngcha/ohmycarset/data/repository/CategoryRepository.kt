402e8bdb0bf4b0ec053c7ed1ea6cf60ac5b05874
HyeonSeongKang <gt9697@naver.com>
2023-08-24 22:23:21 +0900

    feat: 카테고리 구현

--- android/app/src/main/java/com/youngcha/ohmycarset/data/repository/CategoryRepository.kt
+++ android/app/src/main/java/com/youngcha/ohmycarset/data/repository/CategoryRepository.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.youngcha.ohmycarset.data.api.CategoriesApiService
+import com.youngcha.ohmycarset.data.dto.Category
 
 class CategoryRepository(private val categoryAPIService: CategoriesApiService) {
 
@@ -13,33 +14,42 @@ class CategoryRepository(private val categoryAPIService: CategoriesApiService) {
     private val _subCategories = MutableLiveData<List<String>>()
     val subCategories: LiveData<List<String>> = _subCategories
 
+    private val _categories = MutableLiveData<List<Category>>()
+    val categories: LiveData<List<Category>> = _categories
+
     suspend fun getCategories() {
         try {
             val response = categoryAPIService.getCategories()
             if (response.message == "success") {
                 // Filter out categories with ids 1-6, extract their names, and sort them
                 val sortedCategoryNames = response.data.categories
-                    .filter { it.id in 1..6 }
+                    .filter { it.id in 1..5 }
                     .sortedBy { it.id }
                     .map { it.name }
 
                 // "옵션 선택" and "견적 내기" items
-                val mainCategoriesName = sortedCategoryNames + listOf("옵션 선택", "견적 내기")
+                val mainCategoriesName = sortedCategoryNames + listOf("휠 선택", "옵션 선택", "견적 내기")
 
                 val subCategoriesName = response.data.categories
-                    .filterNot { it.id in 1..6 } // We use filterNot here to get categories excluding ids 1-6
+                    .filter { it.id in 7..10 }  // Here we change to get IDs from 7 to 10
                     .sortedBy { it.id }
                     .map { it.name }
 
-                _mainCategories.value = mainCategoriesName
-                _subCategories.value = subCategoriesName
+                val sortedCategories = response.data.categories
+                    .filter { it.id in 7..10 }  // And here as well
+                    .sortedBy { it.id }
+
+                _mainCategories.postValue(mainCategoriesName)
+                _subCategories.postValue(subCategoriesName)
+                _categories.postValue(sortedCategories)
             } else {
-                _mainCategories.value = emptyList()
-                _subCategories.value = emptyList()
+                _mainCategories.postValue(emptyList())
+                _subCategories.postValue(emptyList())
             }
         } catch (e: Exception) {
-            _mainCategories.value = emptyList()
-            _subCategories.value = emptyList()
+            _mainCategories.postValue(emptyList())
+            _subCategories.postValue(emptyList())
         }
     }
+
 }
