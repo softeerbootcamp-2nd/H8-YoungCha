92b89eaa7f4a4d5da4cb896fcde1cec39e9236dd
HyeonSeongKang <gt9697@naver.com>
2023-08-24 22:24:56 +0900

    refactor: 뷰모델 팩토리 카테고리 API 추가

--- android/app/src/main/java/com/youngcha/ohmycarset/viewmodel/factory/CarCustomizationViewModelFactory.kt
+++ android/app/src/main/java/com/youngcha/ohmycarset/viewmodel/factory/CarCustomizationViewModelFactory.kt
@@ -2,13 +2,17 @@ package com.youngcha.ohmycarset.viewmodel.factory
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.youngcha.ohmycarset.data.repository.CategoryRepository
 import com.youngcha.ohmycarset.data.repository.SelfModeRepository
 import com.youngcha.ohmycarset.viewmodel.CarCustomizationViewModel
 
-class CarCustomizationViewModelFactory (private val repository: SelfModeRepository) : ViewModelProvider.Factory {
+class CarCustomizationViewModelFactory (
+    private val selfModeRepository: SelfModeRepository,
+    private val categoryRepository: CategoryRepository
+) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(CarCustomizationViewModel::class.java)) {
-            return CarCustomizationViewModel(repository) as T
+            return CarCustomizationViewModel(selfModeRepository, categoryRepository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
