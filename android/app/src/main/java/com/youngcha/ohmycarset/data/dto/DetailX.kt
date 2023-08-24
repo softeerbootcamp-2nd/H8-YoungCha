5d9493083888781ad23d51665002f7ab641174d5
HyeonSeongKang <gt9697@naver.com>
2023-08-25 06:13:26 +0900

    chore: 가이드 모드 파라미터 클래스 생성

--- android/app/src/main/java/com/youngcha/ohmycarset/data/dto/DetailX.kt
+++
@@ -1,8 +0,0 @@
-package com.youngcha.ohmycarset.data.dto
-
-data class DetailX(
-    val description: String,
-    val imgUrl: Any,
-    val name: Any,
-    val specs: List<Any>
-)
---
+++ android/app/src/main/java/com/youngcha/ohmycarset/data/model/GuideParam.kt
@@ -0,0 +1,3 @@
+package com.youngcha.ohmycarset.data.model
+
+data class GuideParam(val id: Int, val gender: String, val age: Int, val keyword1Id: Int, val keyword2Id: Int, val keyword3Id: Int)
