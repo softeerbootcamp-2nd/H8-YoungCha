e304b2e5812daed9903a1a34e11930abad89640b
HyeonSeongKang <gt9697@naver.com>
2023-08-25 20:00:23 +0900

    chore: 가이드 뷰모델 팩토리 인자 추가

--- android/app/src/main/java/com/youngcha/ohmycarset/viewmodel/factory/GuideModeViewModelFactory.kt
+++ android/app/src/main/java/com/youngcha/ohmycarset/viewmodel/factory/GuideModeViewModelFactory.kt
@@ -2,6 +2,7 @@ package com.youngcha.ohmycarset.viewmodel.factory
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.youngcha.ohmycarset.data.model.GuideParam
 import com.youngcha.ohmycarset.data.repository.CategoryRepository
 import com.youngcha.ohmycarset.data.repository.GuideModeRepository
 import com.youngcha.ohmycarset.viewmodel.CarCustomizationViewModel
@@ -9,11 +10,12 @@ import com.youngcha.ohmycarset.viewmodel.GuideModeViewModel
 
 class GuideModeViewModelFactory(
     private val guideModeRepository: GuideModeRepository,
-    private val categoryRepository: CategoryRepository
+    private val categoryRepository: CategoryRepository,
+    private val guideParam: GuideParam
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(GuideModeViewModel::class.java)) {
-            return GuideModeViewModel(guideModeRepository, categoryRepository) as T
+            return GuideModeViewModel(guideModeRepository, categoryRepository, guideParam) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
