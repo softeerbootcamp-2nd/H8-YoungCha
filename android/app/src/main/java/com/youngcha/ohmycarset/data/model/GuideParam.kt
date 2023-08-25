d963abdcb364f7503b9dfee72f66279cba3792c8
HyeonSeongKang <gt9697@naver.com>
2023-08-25 16:27:10 +0900

    feat: GuideParam 구현

--- android/app/src/main/java/com/youngcha/ohmycarset/data/model/GuideParam.kt
+++ android/app/src/main/java/com/youngcha/ohmycarset/data/model/GuideParam.kt
@@ -1,3 +1,14 @@
 package com.youngcha.ohmycarset.data.model
 
-data class GuideParam(val id: Int, val gender: String, val age: Int, val keyword1Id: Int, val keyword2Id: Int, val keyword3Id: Int)
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class GuideParam(
+    val id: Int,
+    val gender: String,
+    val age: Int,
+    val keyword1Id: Int,
+    val keyword2Id: Int,
+    val keyword3Id: Int
+) : Parcelable
