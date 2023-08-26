fbb9e11b71dffb417de6f817fbd689608116a92e
jhyep <pjh1115_@naver.com>
2023-08-26 16:59:15 +0900

    feat: 헤더 로고에 내 차 만들기 종료 팝업 연결

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,10 +1,12 @@
 import { useCallback, useEffect, useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { useModal } from '@/hooks/useModal';
+import { ModeType } from '@/types';
 import DictionaryToggleButton from './DictionaryToggleButton';
 import ModelChangeButton from './ModelChangeButton';
-import * as Icon from '../../assets/icons';
 import ModeChangeButton from './ModeChangeButton';
-import { ModeType } from '@/types';
+import ExitPopUp from './HeaderPopUp/ExitPopUp';
+import * as Icon from '../../assets/icons';
 
 interface HeaderProps {
   mode?: ModeType;
@@ -15,6 +17,7 @@ const TEXT_MAKING_MY_CAR = '내 차 만들기';
 
 function Header({ mode = 'none', setMode = () => {} }: HeaderProps) {
   const [backgroundColor, setBackgroundColor] = useState('');
+  const { isOpen, openPopUp, closePopUp } = useModal();
 
   const handleScroll = useCallback(() => {
     if (window.scrollY === 0) setBackgroundColor('');
@@ -46,9 +49,19 @@ function Header({ mode = 'none', setMode = () => {} }: HeaderProps) {
     >
       <div className="flex items-center justify-between h-full max-w-5xl mx-auto xl:max-w-none xl:px-96px">
         <div className="flex h-24px gap-20px">
-          <Link to="/">
-            <Icon.HDLogo className="w-45px lg:w-166px h-23px" />
-          </Link>
+          <Icon.HDLogo
+            className="w-45px lg:w-166px h-23px"
+            onClick={() => {
+              if (mode !== 'none') {
+                openPopUp();
+              }
+            }}
+          />
+          {isOpen && mode !== 'none' ? (
+            <ExitPopUp closePopUp={closePopUp} />
+          ) : (
+            <></>
+          )}
           <span>|</span>
 
           <div className={`flex font-medium title3 gap-8px `}>
