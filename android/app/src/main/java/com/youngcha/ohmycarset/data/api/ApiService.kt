2e93aeb61e7f1631b75e19fb25944bb4c3cc7585
HR <dev.muun07@gmail.com>
2023-08-26 20:18:32 +0900

    feat: 차량에 대한 데이터와, 트림에 대한 데이터를 받아오도록 엔드 포인트 지정하기

--- android/app/src/main/java/com/youngcha/ohmycarset/data/api/ApiService.kt
+++ android/app/src/main/java/com/youngcha/ohmycarset/data/api/ApiService.kt
@@ -1,18 +1,31 @@
 package com.youngcha.ohmycarset.data.api
 
 import com.youngcha.ohmycarset.data.model.Dictionary
-import com.youngcha.ohmycarset.data.model.TrimMainData
+import com.youngcha.ohmycarset.data.dto.OptionCategory
+import com.youngcha.ohmycarset.data.dto.TrimDefaultOption
+import com.youngcha.ohmycarset.data.dto.TrimMainData
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
+
 
 interface ApiService {
-    // CarMake Dictionary Endpoint
     @GET("/car-make/dictionary")
     suspend fun getCarMakeDictionary(): Response<Dictionary>
 
     @GET("/cars/{id}/details")
-    suspend fun getMainPageData(@Path("id") id:Int ) :Response<TrimMainData>
+    suspend fun getMainPageData(@Path("id") id: Int): Response<TrimMainData>
 
+    @GET("/trims/{id}/default-components")
+    suspend fun getTrimBasicOption(
+        @Path("id") id: Int,
+        @Query("categoryId") categoryId: Int,
+        @Query("page") page: Int,
+        @Query("size") size: Int
+    ): Response<TrimDefaultOption>
+
+    @GET("/categories")
+    suspend fun getOptionCategory():Response<OptionCategory>
 
 }
