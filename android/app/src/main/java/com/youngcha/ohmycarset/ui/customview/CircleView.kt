e5f32d0b04e1c928a32892fca78d73239b4a8520
HyeonSeongKang <gt9697@naver.com>
2023-08-27 22:47:09 +0900

    chore: 360도 커스텀 뷰 표시

--- android/app/src/main/java/com/youngcha/ohmycarset/ui/customview/CircleView.kt
+++ android/app/src/main/java/com/youngcha/ohmycarset/ui/customview/CircleView.kt
@@ -18,8 +18,16 @@ class CircleView@JvmOverloads constructor(
         isAntiAlias = true
     }
 
+    private val textPaint = Paint().apply {
+        isAntiAlias = true
+        textAlign = Paint.Align.CENTER
+    }
+
     private var borderColor = Color.TRANSPARENT
     private var borderWidth = 0f
+    private var text: String = ""
+    private var textColor: Int = Color.WHITE
+    private var textSize: Float = 30f  // 기본 텍스트 크기를 30f로 설정
 
     init {
         attrs?.let {
@@ -27,6 +35,13 @@ class CircleView@JvmOverloads constructor(
             borderColor = typedArray.getColor(R.styleable.CircleCustomView_borderColor, Color.TRANSPARENT)
             borderWidth = typedArray.getDimension(R.styleable.CircleCustomView_borderWidth, 0f)
             paint.color = typedArray.getColor(R.styleable.CircleCustomView_fillColor, Color.BLACK)
+            text = typedArray.getString(R.styleable.CircleCustomView_circleText) ?: ""
+            textColor = typedArray.getColor(R.styleable.CircleCustomView_textColor, Color.WHITE)
+            textSize = typedArray.getDimension(R.styleable.CircleCustomView_textSize, 30f)
+
+            textPaint.color = textColor
+            textPaint.textSize = textSize
+
             typedArray.recycle()
         }
     }
@@ -46,6 +61,10 @@ class CircleView@JvmOverloads constructor(
         paint.style = Paint.Style.FILL
         paint.color = (paint.color and 0x00FFFFFF) or 0xFF000000.toInt() // 배경색
         canvas.drawCircle(cx, cy, radius - borderWidth, paint)
+
+        // 텍스트 그리기
+        val yOffset = (textPaint.descent() + textPaint.ascent()) / 2
+        canvas.drawText(text, cx, cy - yOffset, textPaint)
     }
 
     fun setFillColor(color: Int) {
@@ -56,6 +75,21 @@ class CircleView@JvmOverloads constructor(
     fun setBorder(color: Int, width: Float) {
         borderColor = color
         borderWidth = width
+        invalidate()
+    }
+
+    fun setText(value: String) {
+        text = value
+        invalidate()
+    }
+
+    fun setTextColor(color: Int) {
+        textPaint.color = color
+        invalidate()
+    }
+
+    fun setTextSize(size: Float) {
+        textPaint.textSize = size
         invalidate()
     }
 }
