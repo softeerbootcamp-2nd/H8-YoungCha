fe7da9675b91d0f06b7092672988696c0cbffa7c
bae-sh <b5460881@naver.com>
2023-08-28 14:08:29 +0900

    chore: svg 대신 hr 태그 변경

--- client/src/pages/making/detail-option-box/DetailOptionHeader.tsx
+++ client/src/pages/making/detail-option-box/DetailOptionHeader.tsx
@@ -17,15 +17,7 @@ function DetailOptionHeader({ option }: DetailOptionHeaderProps) {
         </span>
       </div>
       <div>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="1024"
-          height="2"
-          viewBox="0 0 1024 2"
-          fill="none"
-        >
-          <path d="M0 1H1024" stroke="#A5ACB8" />
-        </svg>
+        <hr />
       </div>
     </>
   );
