7c4252a97ae7c243fe35a91674971f260a102f9a
jhyep <pjh1115_@naver.com>
2023-08-28 14:06:25 +0900

    chore: Icon import 하도록 수정

--- client/src/components/MakingModeButton/index.tsx
+++ client/src/components/MakingModeButton/index.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { TrimType } from '@/types';
+import { formatPrice } from '@/utils';
+import * as Icon from '@/assets/icons';
 import DetailCard from '@/components/MakingModeButton/DetailCard.tsx';
-import ChevronRight from '@/assets/icons/popup/ChevronRight';
 import GuideModeDetailList from '@/components/MakingModeButton/GuideModeDetailList';
 import MainOptionList from '@/components/MakingModeButton/MainOptionList';
-import { formatPrice } from '@/utils';
-import { TrimType } from '@/types';
 
 // TYPES
 export interface MakingModeButtonProps
@@ -55,7 +55,7 @@ function MakingModeButton({
           <div className="title2 mb-16px">{name}</div>
           <div className="flex items-center justify-between">
             <span className="opacity-80">{formatPrice(price)} 부터</span>
-            <ChevronRight />
+            <Icon.ChevronRight />
           </div>
         </button>
       </Link>
